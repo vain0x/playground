113e551a8bcbe52242ee1abe5ac81468c5aef15d
vain0x <vainzerox@gmail.com>
2015-08-27 22:46:13 +0900

    WIP

---
+++ KlacFs/StEval.fs
@@ -0,0 +1,29 @@
+﻿module StEval
+
+    open AST
+    open Value
+    open Basis.Core
+    open Util
+
+    let try_lookup env name =
+        env |> Map.tryFind name
+
+    let rec value_from_expr env =
+        function
+        | IntLit n -> Value.Int n
+        | Ident name ->
+            try_lookup env name
+            |> Option.getOrElse (fun() -> failwith <| "Undefined identifier: " + name)
+        | IdentPtnLit name ->
+            Value.IdentPtn name
+        | ListLit ls ->
+            ls |> List.map (value_from_expr env) |> Value.List
+        | DictLit dt ->
+            dt |> Map.map (konst <| value_from_expr env) |> Value.Dict 
+        | AppPr (f, x) ->
+            Thunk (x, lazy value_from_expr env x)
+        | AppPo
+        | If _ -> failwith "Unsupported"
+
+    let st_eval env =
+        ()
---
+++ KlacFs/Value.fs
@@ -0,0 +1,10 @@
+﻿module Value
+
+    type Object =
+        | Thunk of AST.Expr * Lazy<Object>
+        | Bool of bool
+        | Int of int
+        | IdentPtn of string
+        | List of (Object list)
+        | Dict of Map<string, Object>
+        | InternalFunc of (Object list -> Object)
