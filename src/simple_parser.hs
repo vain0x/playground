b87c2c4fef368a194110e63ebb93c5489001f48d
vain0x <vainzerox@gmail.com>
2015-09-08 22:08:15 +0900

    [parser] Impl parsing lisp value

--- src/simple_parser.hs
+++ src/simple_parser.hs
@@ -1,5 +1,15 @@
+import Control.Monad
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal
+	= Atom String
+	| List [LispVal]
+	| DottedList [LispVal] LispVal
+	| Number Integer
+	| String String
+	| Bool Bool
+	deriving (Show)
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -7,11 +17,37 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseAtom :: Parser LispVal
+parseAtom = do
+	first <- letter <|> symbol
+	rest <- many (letter <|> symbol <|> digit)
+	let atom = first : rest
+	return $ case atom of
+		"#t" -> Bool True
+		"#f" -> Bool False
+		_    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseString :: Parser LispVal
+parseString = do
+	char '"'
+	x <- many (noneOf "\"")
+	char '"'
+	return $ String x
+
+parseExpr :: Parser LispVal
+parseExpr =
+	    parseAtom
+	<|> parseNumber
+	<|> parseString
+
 readExpr :: String -> String
 readExpr input =
-	case parse (spaces >> symbol) "lisp" input of
+	case parse parseExpr "lisp" input of
 		Left err -> "No match: " ++ show err
-		Right val -> "Found value"
+		Right val -> "Found value: " ++ show val
 
 main :: IO ()
 main = do
