bc12f41b886aa87c150e508891d0533533385946
vain0x <vainzerox@gmail.com>
2015-09-09 07:45:48 +0900

    Redefine show :: LispVal -> String

--- src/LispVal.hs
+++ src/LispVal.hs
@@ -2,10 +2,29 @@ module LispVal where
 
 data LispVal
 	= Atom String
-	| List [LispVal]
-	| DottedList [LispVal] LispVal
 	| Number Integer
 	| String String
 	| Char Char
 	| Bool Bool
-	deriving (Show)
+	| List [LispVal]
+	| DottedList [LispVal] LispVal
+
+showVal :: LispVal -> String
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (String contents) = show contents
+showVal (Char '\n') = "#\\newline"
+showVal (Char ' ') = "#\\space"
+showVal (Char c) = "#\\" ++ [c]
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List xs) =
+	"(" ++ unwordsList xs ++ ")"
+showVal (DottedList head tail) =
+	"(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+instance Show LispVal where
+	show = showVal
