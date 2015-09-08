d2b38a5a9581660a37017378c9bd0b7564500540
vain0x <vainzerox@gmail.com>
2015-09-08 22:02:48 +0900

    [parser] Ignore spaces

--- src/simple_parser.hs
+++ src/simple_parser.hs
@@ -4,10 +4,14 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
-	Left err -> "No match: " ++ show err
-	Right val -> "Found value"
+readExpr input =
+	case parse (spaces >> symbol) "lisp" input of
+		Left err -> "No match: " ++ show err
+		Right val -> "Found value"
 
 main :: IO ()
 main = do
