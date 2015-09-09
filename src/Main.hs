7e0330cf6055ba3f13a4bb28d949062eaf892d7a
vain0x <vainzerox@gmail.com>
2015-09-09 23:48:50 +0900

    Impl REPL

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import System.IO
 import System.Environment
 import Control.Monad
 
@@ -7,8 +8,33 @@ import LispVal
 import Parser
 import Eval
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr =
+	return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr =
+	evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred next action = do 
+	result <- next
+	if pred result 
+		then return ()
+		else action result >> until_ pred next action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
 main :: IO ()
 main = do
-	expr <- fmap head getArgs
-	result <- return $ liftM show $ readExpr expr >>= eval
-	putStrLn $ extractValue $ trapError result
+	args <- getArgs
+	case args of
+		[] -> runRepl
+		expr : [] -> evalAndPrint expr
