605131c426289a5d5baf041086903869dd7d09de
vain0x <vainzerox@gmail.com>
2015-10-05 20:56:46 +0900

    Commit script written in <http://qiita.com/hachimitu22/items/f022baab18ee8d267a08>

---
+++ PopTranslateEnJa.py
@@ -0,0 +1,54 @@
+# coding=UTF-8
+import sublime, sublime_plugin
+import urllib.request
+import xml.etree.ElementTree as ET
+
+class popTranslateEnglishIntoJapaneseCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        search_word = self.view.substr(self.view.sel()[0])
+        if search_word == "":
+            print('none select word')
+            return
+        else:
+            print(search_word)
+
+        item_id = self.getItemID(search_word)
+        text = self.getTranslatedText(item_id)
+        if text == '':
+            return
+
+        text_arr = self.splitTranslatedText(text, '\t')
+        self.view.show_popup_menu(text_arr, None)
+
+
+    def getXmlElementText(self, url, tag):
+        print('url : ' + url)
+        try:
+            xml = urllib.request.urlopen(url)
+        except urllib.error.HTTPError as e:
+            print('error code : ' + str(e.code))
+            print('error read : ' + str(e.read()))
+            return ''
+
+        print(xml)
+        tree = ET.parse(xml)
+        root = tree.getroot()
+        element = root.find('.//{http://btonic.est.co.jp/NetDic/NetDicV09}' + tag)
+        text = element.text
+        print(text)
+        return text
+
+    def getItemID(self, search_word):
+        head = 'http://public.dejizo.jp/NetDicV09.asmx/SearchDicItemLite?Dic=EJdict&Word='
+        end = '&Scope=HEADWORD&Match=EXACT&Merge=OR&Prof=XHTML&PageSize=20&PageIndex=0'
+        url = head + search_word + end
+        return self.getXmlElementText(url, 'ItemID')
+
+    def getTranslatedText(self, item_id):
+        head = 'http://public.dejizo.jp/NetDicV09.asmx/GetDicItemLite?Dic=EJdict&Item='
+        end = '&Loc=&Prof=XHTML'
+        url = head + item_id + end
+        return self.getXmlElementText(url, 'Body/div/div')
+
+    def splitTranslatedText(self, translated_text, split_word):
+        return translated_text.split(split_word)
