e58937215fb4ee8cddc51469d6eec27edfbcb176
vain0x <vainzerox@gmail.com>
2016-02-04 19:41:47 +0900

    Remove auto-generated code

--- AdhocFs/Program.fs
+++ AdhocFs/Program.fs
@@ -1,7 +1,7 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =
+
+  // exit code
+  0
