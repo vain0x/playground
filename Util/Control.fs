4422ff431bdc279df62314180c16e7d0460d530b
vain0x <vainzerox@gmail.com>
2016-04-02 14:27:02 +0900

    Moving code, which solves problem

--- Util/Control.fs
+++ Util/Control.fs
@@ -76,34 +76,3 @@ module UpdateMonad =
         )
 
   let update = UpdateBuilder()
-
-[<AutoOpen>]
-module StateMonad =
-  open Basis.Core
-
-  type StateState<'t> =
-    | State of 't
-
-  type StateUpdate<'t> = 
-    | StateUpdate of option<'t>
-
-    static member Empty = StateUpdate None
-
-    static member Append(StateUpdate l, StateUpdate r) = 
-      match l, r with 
-      | None, x
-      | x, None -> x |> StateUpdate
-      | Some _, Some s -> Some s |> StateUpdate
-
-    static member Update(s, StateUpdate u) = 
-      u |> Option.getOr s
-
-  [<RequireQualifiedAccess>]
-  module State =
-    let run (State s) = s
-
-    let put s  = Update (fun _ -> (StateUpdate (Some (State s)), ()))
-    let get () = Update (fun s -> (StateUpdate None, run s))
-
-    let eval s m =
-      Update.run m (State s) |> snd
