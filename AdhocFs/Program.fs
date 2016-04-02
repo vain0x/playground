7541e9f55dd7767a6899a719be3c4aec073361cc
vain0x <vainzerox@gmail.com>
2016-04-02 14:20:36 +0900

    WIP

--- AdhocFs/Program.fs
+++ AdhocFs/Program.fs
@@ -1,7 +1,25 @@
 ﻿open System
 
+open Util.Control
+
 [<EntryPoint>]
 let main argv =
+  
+  let inc =
+    update {
+      let! x = State.get ()
+      do! State.put (x + 1)
+    }
+
+  let x =
+    update {
+      for i in 1..10 do
+        do! inc
+      return! State.get ()
+    }
+    |> State.eval 0
+
+  printfn "%A" x
 
   // exit code
   0
