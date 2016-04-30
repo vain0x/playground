9dc2ca9a4e0afb9574fa41370eeae44e3abf81e6
vain0x <vainzerox@gmail.com>
2016-04-30 19:03:40 +0900

    Enable to open settings

--- Dyxi.Muse.ViewModel/MainWindow.cs
+++ Dyxi.Muse.ViewModel/MainWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Dyxi.CSharp.Util;
 
 namespace Dyxi.Muse.ViewModel
@@ -12,6 +13,12 @@ namespace Dyxi.Muse.ViewModel
         public MainWindow()
         {
             _settingsWindow = new SettingsWindow();
+        }
+
+        private ICommand _openSettingsCommand;
+        public ICommand OpenSettingsCommand
+        {
+            get { return (_openSettingsCommand ?? (_openSettingsCommand = new LambdaCommand(param => SettingsWindow.Show(NullableUnit.Instance)))); }
         }
 
         public SettingsWindow SettingsWindow
