3ee9aefb54eeb718fea2799fa14735d32b2de0c8
vain0x <vainzerox@gmail.com>
2016-04-30 19:10:06 +0900

    Add Model.Entity

---
+++ Dyxi.Muse.Model/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dyxi.Muse.Model
+{
+    public static class Entity
+    {
+        public static dyxi_museEntities Instance = new dyxi_museEntities();
+    }
+}
