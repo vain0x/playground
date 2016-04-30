d552eb3ed7d527aac5cae1db97f14ba7c64b6017
vain0x <vainzerox@gmail.com>
2016-04-30 18:23:34 +0900

    Add NullableUnit class

---
+++ Dyxi.CSharp.Util/Misc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dyxi.CSharp.Util
+{
+    public class NullableUnit
+    {
+        private static NullableUnit _instance = null;
+        public static NullableUnit Instance
+        {
+            get { return (_instance ?? (_instance = new NullableUnit())); }
+        }
+    }
+}
