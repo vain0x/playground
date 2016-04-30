4852a56909dcf080c24414f96b8dfe37703df39e
vain0x <vainzerox@gmail.com>
2016-04-30 19:08:09 +0900

    Add ViewModel.MainWindow.Tracks

--- Dyxi.Muse.ViewModel/MainWindow.cs
+++ Dyxi.Muse.ViewModel/MainWindow.cs
@@ -21,8 +21,29 @@ namespace Dyxi.Muse.ViewModel
         {
             get { return (_openSettingsCommand ?? (_openSettingsCommand = new LambdaCommand(param => SettingsWindow.Show(NullableUnit.Instance)))); }
         }
+
+        private IColl _coll;
+        public IColl Coll
+        {
+            get { return _coll; }
+            set
+            {
+                _coll = value;
+                RaisePropertyChanged();
+                Tracks = _coll.Items.ToArray();
+            }
+        }
         
-        public IColl Coll { get; set; }
+        private media[] _tracks;
+        public media[] Tracks
+        {
+            get { return _tracks; }
+            set
+            {
+                _tracks = value;
+                RaisePropertyChanged();
+            }
+        }
 
         public SettingsWindow SettingsWindow
         {
