d9800ffe8d3706494eabe829180e1b23c3df63b5
vain0x <vainzerox@gmail.com>
2016-04-30 18:17:30 +0900

    Add DialogViewModelBase

--- Dyxi.CSharp.Util/ViewModelBase.cs
+++ Dyxi.CSharp.Util/ViewModelBase.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Dyxi.CSharp.Util
 {
@@ -19,5 +21,53 @@ namespace Dyxi.CSharp.Util
             if (PropertyChanged == null) return;
             PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
+    }
+
+    public class DialogViewModelBase<T> : ViewModelBase
+        where T : class
+    {
+        public Visibility Visibility
+        {
+            get { return _data == null ? Visibility.Collapsed : Visibility.Visible; }
+        }
+
+        public T Data
+        {
+            get { return _data; }
+            set
+            {
+                if (_data != value)
+                {
+                    _data = value;
+                    RaisePropertyChanged("Visibility");
+                }
+            }
+        }
+
+        public void Show(T x)
+        {
+            if (x == null) throw new ArgumentNullException();
+            Data = x;
+        }
+
+        public bool Hide()
+        {
+            if (Data == null)
+            {
+                return false;
+            }
+            else
+            {
+                Data = null;
+                return true;
+            }
+        }
+
+        public void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = Hide();
+        }
+
+        private T _data = null;
     }
 }
