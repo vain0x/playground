8503539d8013276f27a8aaaac1502471dae28709
vain0x <vainzerox@gmail.com>
2016-04-30 18:38:48 +0900

    Add SettingsWindow

---
+++ Dyxi.Muse.View/SettingsWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dyxi.Muse.View
+{
+    /// <summary>
+    /// SettingsWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow(ViewModel.SettingsWindow vm)
+        {
+            InitializeComponent();
+
+            this.DataContext = _vm = vm;
+        }
+
+        public void Window_Closing(object sender, CancelEventArgs e)
+        {
+            _vm.OnClosing(e);
+        }
+
+        private ViewModel.SettingsWindow _vm;
+    }
+}
---
+++ Dyxi.Muse.ViewModel/SettingsWindow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dyxi.CSharp.Util;
+
+namespace Dyxi.Muse.ViewModel
+{
+    public class SettingsWindow
+        : DialogViewModelBase<NullableUnit>
+    {
+    }
+}
