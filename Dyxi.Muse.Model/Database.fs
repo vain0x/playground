b3b1aee77bfe9e6b981791dd96ad05bd92900930
vain0x <vainzerox@gmail.com>
2016-04-30 10:09:50 +0900

    Add Database.fs

---
+++ Dyxi.Muse.Model/Database.fs
@@ -0,0 +1,15 @@
+﻿[<AutoOpen>]
+module Dyxi.Muse.Model.Database
+
+open FSharp.Data.Sql
+
+type sql =
+  SqlDataProvider<
+      Common.DatabaseProviderTypes.MYSQL
+    , ConnectionString  = "Data Source=localhost; user=root; password=;"
+    , UseOptionTypes    = true
+    , Owner = "dyxi_muse"
+    >
+
+let ctx = sql.GetDataContext()
+let db = ctx.DyxiMuse
