ca1fc56dedb0cbdb9a8d3eb5c2f1d5eb24faf419
vain0x <vainzerox@gmail.com>
2016-04-30 18:44:28 +0900

    Add IColl

---
+++ Dyxi.Muse.Model/Types.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dyxi.Muse.Model
+{
+    public interface IColl
+    {
+        string Name { get; }
+        IEnumerable<media> Items { get; }
+    }
+
+    public class MusicColl : IColl
+    {
+        private static MusicColl _instance;
+        public static MusicColl Instance
+        {
+            get { return (_instance ?? (_instance = new MusicColl())); }
+        }
+        
+        public string Name
+        {
+            get { return "Music"; }
+        }
+
+        public IEnumerable<media> Items
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
