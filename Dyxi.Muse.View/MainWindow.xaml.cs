7a03ee143e5b1a78d18b1eea7c0d3a3a2813e744
vain0x <vainzerox@gmail.com>
2016-04-30 18:08:44 +0900

    Make main window mock

--- Dyxi.Muse.View/MainWindow.xaml.cs
+++ Dyxi.Muse.View/MainWindow.xaml.cs
@@ -23,6 +23,36 @@ namespace Dyxi.Muse.View
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = this._vm = new ViewModel.MainWindow();
+
+            this._musicColl.DataContext = Muse.Model.MusicColl.Instance;
+
+            this._settingsWindow = new SettingsWindow(this._vm.SettingsWindow);
         }
+
+        private void _collTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var item = e.NewValue as TreeViewItem;
+            var coll = (item != null ? item.DataContext as Model.IColl : null);
+            if (coll == null) return;
+            // TODO: リストビューの項目源を coll にする
+        }
+
+        private void _listView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var media = ((FrameworkElement)e.OriginalSource).DataContext as Model.media;
+            if (media == null) return;
+            // TODO: スタックに media を積む
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            _vm.SettingsWindow.Hide();
+            _settingsWindow.Close();
+        }
+
+        private ViewModel.MainWindow _vm;
+        private SettingsWindow _settingsWindow;
     }
 }
---
+++ Dyxi.Muse.ViewModel/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dyxi.CSharp.Util;
+
+namespace Dyxi.Muse.ViewModel
+{
+    public class MainWindow : ViewModelBase
+    {
+        public MainWindow()
+        {
+            _settingsWindow = new SettingsWindow();
+        }
+
+        public SettingsWindow SettingsWindow
+        {
+            get { return _settingsWindow; }
+        }
+
+        private SettingsWindow _settingsWindow;
+    }
+}
