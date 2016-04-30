1bc09e92564cb51429a29b1dd19f84d59886350d
vain0x <vainzerox@gmail.com>
2016-04-30 20:19:03 +0900

    Add TryFindPeopleByName

--- Dyxi.Muse.Model/Entity.cs
+++ Dyxi.Muse.Model/Entity.cs
@@ -9,5 +9,10 @@ namespace Dyxi.Muse.Model
     public static class Entity
     {
         public static dyxi_museEntities Instance = new dyxi_museEntities();
+
+        public static people TryFindPeopleByName(string name)
+        {
+            return Instance.peoples.Where(people => people.name == name).FirstOrDefault();
+        }
     }
 }
