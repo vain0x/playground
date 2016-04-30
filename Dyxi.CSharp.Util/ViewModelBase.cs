26baf50361c02dc72cd4947533d8c3847eb124b2
vain0x <vainzerox@gmail.com>
2016-04-30 18:04:25 +0900

    Add ViewModelBase

---
+++ Dyxi.CSharp.Util/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dyxi.CSharp.Util
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Debug.Assert(propertyName != null);
+            if (PropertyChanged == null) return;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
