3afa9b6ec7acdd2049d61f72858d681edc279688
vain0x <vainzerox@gmail.com>
2016-05-01 10:09:55 +0900

    Add PlayCommand

--- Dyxi.Muse.ViewModel/MainWindow.cs
+++ Dyxi.Muse.ViewModel/MainWindow.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using NAudio.Wave;
 using Dyxi.CSharp.Util;
 using Dyxi.Muse.Model;
 
@@ -15,6 +17,10 @@ namespace Dyxi.Muse.ViewModel
         {
             _settingsWindow = new SettingsWindow();
             _stack = new List<TrackRow>();
+            _playingTrack = null;
+
+            _waveOut = new WaveOut();
+            _waveOut.PlaybackStopped += PlaybackStopped;
         }
         
         private ICommand _openSettingsCommand;
@@ -69,6 +75,54 @@ namespace Dyxi.Muse.ViewModel
             Stack.Insert(0, row);
             MediaCache.FetchAsync(row.MediaId);
             RaisePropertyChanged("Stack");
+        }
+
+        private WaveOut _waveOut;
+        private TrackRow _playingTrack;
+
+        private ICommand _playCommand;
+        public ICommand PlayCommand
+        {
+            get { return (_playCommand ?? (_playCommand = new LambdaCommand(param => Play()))); }
+        }
+
+        private void Play()
+        {
+            if (_playingTrack == null)
+            {
+                if (Stack.IsEmpty()) return;
+                _playingTrack = Stack.First();
+                var task = MediaCache.FetchAsync(_playingTrack.MediaId);
+                task.Wait();
+                var reader = new MediaFoundationReader(task.Result);
+                _waveOut.Init(reader);
+                _waveOut.Play();
+            }
+            else
+            {
+                switch (_waveOut.PlaybackState)
+                {
+                    case PlaybackState.Paused:
+                        _waveOut.Resume();
+                        break;
+                    case PlaybackState.Playing:
+                        _waveOut.Pause();
+                        break;
+                }
+            }
+        }
+
+        private void PlaybackStopped(object sender, EventArgs e)
+        {
+            switch (_waveOut.PlaybackState)
+            {
+                case PlaybackState.Stopped:
+                    _playingTrack = null;
+                    Stack.Remove(_playingTrack);
+                    RaisePropertyChanged("Stack");
+                    Play();
+                    break;
+            }
         }
         
         public SettingsWindow SettingsWindow
