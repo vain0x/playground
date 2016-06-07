a881bef0d0f99e672f1d92a8cc5b2d084ba97298
vain0x <vainzerox@gmail.com>
2016-06-07 22:17:08 +0900

    Add BTree test

---
+++ AdhocFs/BTree.fs
@@ -0,0 +1,17 @@
+﻿namespace AdhocFs
+
+module BTree =
+  type BTree<'k, 'v> =
+    | BTree
+
+  let empty = BTree
+
+  let length _ = 0
+
+  let tryFind k this = None
+
+  let add k v this = ()
+
+  let remove k this = ()
+
+  let toList this = []
