ee72367bbbc526de3af808b47b42068ea38683c1
vain0x <vainzerox@gmail.com>
2016-08-09 19:47:08 +0900

    Move Join to new file

---
+++ QueryBuilder/SelectBuilder/Detail/Join.vb
@@ -0,0 +1,14 @@
+﻿Namespace Detail
+    Public Class [Join]
+        ' inner join, outer join, etc.
+        Public ReadOnly JoinType As String
+        Public ReadOnly TableName As Name
+        Public ReadOnly OnExpression As String
+
+        Public Sub New(joinType As String, tableName As Name, [on] As String)
+            Me.JoinType = joinType
+            Me.TableName = tableName
+            Me.OnExpression = [on]
+        End Sub
+    End Class
+End Namespace
--- QueryBuilder/SelectBuilder/SelectBuilder.vb
+++ QueryBuilder/SelectBuilder/SelectBuilder.vb
@@ -1,20 +1,8 @@
 ﻿Imports System.Data.Common
+Imports QueryBuilder.Detail
 
 Public Class SelectBuilder
     Inherits BuilderBase(Of SelectBuilder)
-
-    Private Class [Join]
-        ' inner join, outer join, etc.
-        Public ReadOnly JoinType As String
-        Public ReadOnly TableName As Name
-        Public ReadOnly OnExpression As String
-
-        Public Sub New(joinType As String, tableName As Name, [on] As String)
-            Me.JoinType = joinType
-            Me.TableName = tableName
-            Me.OnExpression = [on]
-        End Sub
-    End Class
 
     Private ReadOnly _parameterFactory As IDbParameterFactory
     Private ReadOnly _tables As New List(Of Name)()
