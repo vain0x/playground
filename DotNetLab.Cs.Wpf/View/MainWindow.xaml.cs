89940f51a38397702b51bb1c4d98a3402038d0b7
vain0x <vainzerox@gmail.com>
2016-08-13 23:57:51 +0900

    Emphasis while inputting

--- DotNetLab.Cs.Wpf/View/MainWindow.xaml.cs
+++ DotNetLab.Cs.Wpf/View/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Reactive.Linq;
+using DotNetLab.Cs.Wpf.ViewModel;
 
 namespace DotNetLab.Cs.Wpf
 {
@@ -23,6 +25,8 @@ namespace DotNetLab.Cs.Wpf
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ DotNetLab.Cs.Wpf/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Reactive.Bindings;
+
+namespace DotNetLab.Cs.Wpf.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public ReactiveProperty<string> InputText { get; } =
+            new ReactiveProperty<string>(string.Empty);
+
+        public ReactiveProperty<string> Text { get; }
+
+        public ReactiveProperty<Brush> Foreground { get; } =
+            new ReactiveProperty<Brush>(Brushes.Black);
+
+        public MainWindowViewModel()
+        {
+            Text =
+                InputText
+                .Delay(TimeSpan.FromSeconds(1))
+                .Select(s => "Your input: " + s)
+                .ToReactiveProperty();
+
+            Foreground =
+                InputText
+                .Select(s => (Brush)Brushes.Red)
+                .Merge(
+                    InputText
+                    .Throttle(TimeSpan.FromSeconds(2))
+                    .Select(s => Brushes.Black)
+                    )
+                .ToReactiveProperty();
+        }
+    }
+}
