905ac03b25c31bd91fc546049e60a1faa82bef43
vain0x <vainzerox@gmail.com>
2016-08-31 01:44:51 +0900

    Add SqlExpression.Concat

--- FluentSqlBuilder/SqlSyntax/SqlExpression/SqlExpressionExtensions.cs
+++ FluentSqlBuilder/SqlSyntax/SqlExpression/SqlExpressionExtensions.cs
@@ -7,6 +7,34 @@ namespace FluentSqlBuilder.Public
 {
     public static class SqlExpressionExtensions
     {
+        #region Internal
+        static ISqlExpression<T>
+            Invoke<T>(
+                SqlBuilder sqlBuilder,
+                string functionName,
+                IEnumerable<ISqlExpression<IScalar<object>>> arguments
+            ) =>
+            new CompoundExpression<T>(
+                sqlBuilder,
+                SqlPart.Concat(
+                    new[] { SqlPart.FromToken(functionName) }
+                    .Concat(
+                        arguments
+                        .Intersperse(SqlPart.FromToken(","))
+                        .Enclose(SqlPart.FromToken("("), SqlPart.FromToken(")"))
+                    )));
+        #endregion
+
+        #region Normal operators
+        public static ISqlExpression<IScalar<string>>
+            Concat(
+                this ISqlExpression<IScalar<string>> lhs,
+                params ISqlExpression<IScalar<string>>[] rhs
+            ) =>
+            Invoke<IScalar<string>>(lhs.SqlBuilder, "concat", new[] { lhs }.Concat(rhs));
+        #endregion
+
+        #region Condition operators
         public static ISqlCondition
             IsNull<X>(this ISqlExpression<IScalar<X>> lhs) =>
             new SqlCondition(lhs.SqlBuilder, SqlPart.FromToken("is null"));
@@ -17,5 +45,6 @@ namespace FluentSqlBuilder.Public
                 ISqlExpression<IScalar<X>> rhs
             ) =>
             new SqlCondition(lhs.SqlBuilder, lhs, SqlPart.FromToken("="), rhs);
+        #endregion
     }
 }
