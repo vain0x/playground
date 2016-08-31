8a22775b5fb65c2ba6a0e5972a1ad1e96121c341
vain0x <vainzerox@gmail.com>
2016-08-31 21:13:11 +0900

    Add department_id column to employees table

--- FluentSqlBuilder.Test/FakeDatabase/Employee.cs
+++ FluentSqlBuilder.Test/FakeDatabase/Employee.cs
@@ -8,12 +8,14 @@ namespace FluentSqlBuilder.Test
         public ITable Table { get; }
         public IColumn<string> Name { get; }
         public IColumn<long> Age { get; }
+        public IColumn<long> DepartmentId { get; }
 
         public Employee(SqlBuilder sqlBuilder)
         {
             Table = sqlBuilder.Table("employees");
             Name = Table.Column<string>("name");
             Age = Table.Column<long>("age");
+            DepartmentId = Table.Column<long>("department_id");
         }
     }
 }
