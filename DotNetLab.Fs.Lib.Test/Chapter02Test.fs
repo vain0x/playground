4e64571c195874bfa2626fff6da328c0b2947ae6
vain0x <vainzerox@gmail.com>
2016-10-02 16:20:17 +0900

    Solve Exercise 2.1

---
+++ DotNetLab.Fs.Lib.Test/Chapter02Test.fs
@@ -0,0 +1,20 @@
+﻿namespace DotNetLab.Fs.Lib.PFDS
+
+open Persimmon
+open Persimmon.Syntax.UseTestNameByReflection
+open DotNetLab.Fs.Lib.PFDS.Chapter02
+
+module Chapter02Test =
+  let suffixTest =
+    test {
+      let actual = [1; 2; 3; 4] |> suffix
+      let expected =
+        [
+          [1; 2; 3; 4]
+          [2; 3; 4]
+          [3; 4]
+          [4]
+          []
+        ]
+      do! actual |> assertEquals expected
+    }
---
+++ DotNetLab.Fs.Lib/Chapter02.fs
@@ -0,0 +1,34 @@
+﻿namespace DotNetLab.Fs.Lib.PFDS
+
+module Chapter02 =
+  // Ex2.1
+  let rec suffix =
+    function
+    | x :: xs ->
+      (x :: xs) :: suffix xs
+    | [] ->
+      [[]]
+
+  (*
+    時間計算量 O(n):
+        長さ n のリスト xs について、time(suffix xs) = T(n) とおく。
+        任意の xs: 'x list をとる。n = length xs とおく。
+        xs = [] のとき、
+            T(0) = time(suffix []) = 1
+        xs = x :: xs' のとき、
+            T(n) = time(suffix xs)
+                = time((x :: xs') :: suffix xs')
+                = time(suffix xs') + 2
+                = T(n - 1) + 2
+        従って、T(n) = 2n + 1 = O(n)
+    空間計算量 O(n):
+        長さ n のリスト xs について、space(suffix xs) = S(n) とおく。
+        任意の xs: 'x list をとる。n = length xs とおく。
+        xs = [] のとき
+            S(0) = space([[]]) = 1
+        xs = x :: xs' のとき、xs' は xs の部分リストなので空間 O(1) で表現できて、
+            S(n) = space(suffix xs)
+                = space(x :: xs') + space(suffix xs')
+                = space(suffix xs') + O(1)
+        従って、S(n) = O(n)
+  *)
