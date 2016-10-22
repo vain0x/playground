8c1de7b81e3603c7d65a1c17c90ba8a2e6aa88ee
vain0x <vainzerox@gmail.com>
2016-10-22 22:04:44 +0900

    WIP

---
+++ AsterSql.Core/Engine/Memory/EnvironmentStack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsterSql.Engine.Memory
+{
+    class EnvironmentStack
+    {
+        public Dictionary<string, MemoryRelation> 
+    }
+}
