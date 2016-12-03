7ab362993136e4dbeba0237d804892c32b318d43
vain0x <vainzerox@gmail.com>
2016-12-03 23:54:25 +0900

    Add Dictionary.ofSeq

---
+++ DotNetKit.FSharp/Dictionary.fs
@@ -0,0 +1,11 @@
+﻿namespace DotNetKit.FSharp
+
+open System.Collections.Generic
+
+[<RequireQualifiedAccess>]
+module Dictionary =
+  let ofSeq kvs =
+    let this = Dictionary()
+    for (key, value) in kvs do
+      this.Add(key, value)
+    this
