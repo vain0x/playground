448f0bb64e062f171dfcc301f13917b9bc6af93d
vain0x <vainzerox@gmail.com>
2016-12-03 23:55:09 +0900

    Add Dictionary.tryFind

--- DotNetKit.FSharp/Dictionary.fs
+++ DotNetKit.FSharp/Dictionary.fs
@@ -4,6 +4,13 @@ open System.Collections.Generic
 
 [<RequireQualifiedAccess>]
 module Dictionary =
+  let tryFind key (this: Dictionary<_, _>) =
+    match this.TryGetValue(key) with
+    | (true, value) ->
+      Some value
+    | (false, _) ->
+      None
+
   let ofSeq kvs =
     let this = Dictionary()
     for (key, value) in kvs do
