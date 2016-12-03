9ccc0160259402c43c3ca3745033a62a3eb6c4d1
vain0x <vainzerox@gmail.com>
2016-12-03 23:16:54 +0900

    Add Option module

---
+++ DotNetKit.FSharp/Option.fs
@@ -0,0 +1,13 @@
+﻿namespace DotNetKit.FSharp
+
+[<RequireQualifiedAccess>]
+module Option =
+  let getOr x =
+    function
+    | Some x -> x
+    | None -> x
+
+  let getOrElse getX =
+    function
+    | Some x -> x
+    | None -> getX ()
