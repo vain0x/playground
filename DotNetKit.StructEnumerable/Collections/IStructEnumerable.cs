b194551cebfe7b7206bbe0880fb09b85a4252432
vain0x <vainzerox@gmail.com>
2016-12-12 21:18:50 +0900

    Add IStructEnumerable

---
+++ DotNetKit.StructEnumerable/Collections/IStructEnumerable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetKit.Collections
+{
+    /// <summary>
+    /// Represents a sequence.
+    /// Equivalent to <see cref="IEnumerable{T}"/>
+    /// except the enumerator type is specified and must be a value type.
+    /// <para lang="ja">
+    /// <see cref="IEnumerable{T}"/> と同じく、シーケンスを表す。
+    /// ただし、生成する列挙子の型は特定の値型でなければならない。
+    /// </para>
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TEnumerator"></typeparam>
+    public interface IStructEnumerable<out TValue, out TEnumerator>
+        : IEnumerable<TValue>
+        where TEnumerator : struct, IEnumerator<TValue>
+    {
+        /// <summary>
+        /// Gets a struct enumerator.
+        /// </summary>
+        /// <returns></returns>
+        new TEnumerator GetEnumerator();
+    }
+}
