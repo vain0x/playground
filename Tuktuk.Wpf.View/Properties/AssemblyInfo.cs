29e25106579f0a00e83d1adf57314cf67ec85dc6
vain0x <vainzerox@gmail.com>
2017-01-15 12:02:13 +0900

    Rename Tuktuk.Wpf to Tuktuk.Wpf.View

--- Tuktuk.Wpf.View/Properties/AssemblyInfo.cs
+++ Tuktuk.Wpf.View/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Tuktuk.Wpf")]
+[assembly: AssemblyTitle("Tuktuk.Wpf.View")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tuktuk.Wpf")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Tuktuk.Wpf.View")]
+[assembly: AssemblyCopyright("(C) 2017 vain0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
