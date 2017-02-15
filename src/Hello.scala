4f7ac6b6da97a8d7673402886e948caf22e71ace
vain0x <vainzerox@gmail.com>
2017-02-16 06:54:25 +0900

    Solve exercise in https://dwango.github.io/scala_text/control-syntax.html

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,5 +1,13 @@
+import scala.util._
+import java.security._
+
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val random = new Random(new SecureRandom())
+    for (_ <- 1 to 1000) {
+      val s = random.alphanumeric.take(4).mkString
+      val t = s + s.charAt(0)
+      println(t)
+    }
   }
 }
