56528472dad635bc9ae9f78411b8d90135e1428a
vain0x <vainzerox@gmail.com>
2017-02-16 07:06:19 +0900

    My first class: Point

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,13 +1,23 @@
 import scala.util._
 import java.security._
 
+/**
+  * Represents a point in a plane.
+  * @param x
+  * @param y
+  */
+class Point(val x: Int, val y: Int) {
+  def +(r: Point) = {
+    new Point(x + r.x, y + r.y)
+  }
+
+  override def toString: String = {
+    s"Point(${x}, ${y})"
+  }
+}
+
 object Hello {
   def main(args: Array[String]): Unit = {
-    val random = new Random(new SecureRandom())
-    for (_ <- 1 to 1000) {
-      val s = random.alphanumeric.take(4).mkString
-      val t = s + s.charAt(0)
-      println(t)
-    }
+    println(s"${new Point(1, 2) + new Point(10, 10)}")
   }
 }
