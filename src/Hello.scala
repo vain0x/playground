c7f0a110675e8959d4215a7484784877e929b98f
vain0x <vainzerox@gmail.com>
2017-02-17 03:54:01 +0900

    Solve exercise in https://dwango.github.io/scala_text/collection.html

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,26 +1,46 @@
 import scala.io._
 
 object Hello {
-  def withFile[X](filePath: String)(f: Source => X): X = {
-    val source = Source.fromFile(filePath)
-    try {
-      f(source)
-    } finally {
-      source.close()
-    }
+  def swapArray[X](array: Array[X], i: Int, j: Int) = {
+    val t = array(i)
+    array(i) = array(j)
+    array(j) = t
+  }
+
+  def joinByComma(start: Int, end: Int) = {
+    (start to end).mkString(",")
+  }
+
+  def reverse[X](list: List[X]) = {
+    list.foldLeft[List[X]](Nil)((list, x) => x :: list)
+  }
+
+  def sum(list: List[Int]) = {
+    list.foldRight(0)((x, sum) => x + sum)
+  }
+
+  def mkString[T](list: List[T])(sep: String) = {
+    list.foldLeft(("", true))((t, x) =>  {
+      val (acc, isFirst) = t
+      if (isFirst) {
+        (x.toString, false)
+      } else {
+        (acc + sep + x.toString, false)
+      }
+    })._1
   }
 
   def main(args: Array[String]): Unit = {
-    val currentDirectory = System.getProperty("user.dir")
-    println(s"Current directory: $currentDirectory")
+    val array = Array(0, 1, 2, 3, 4)
+    swapArray(array, 1, 3)
+    println(s"array = ${array.mkString(", ")}")
 
-    withFile("input.txt")(source => {
-        var i = 1
-        for (line <- source.getLines()) {
-          println(s"$i: $line")
-          i += 1
-        }
-      }
-    )
+    println(s"joinByComma(2, 5) = ${joinByComma(2, 5)}")
+
+    println(s"reverse: ${reverse(List(1, 2, 3, 4))}")
+
+    println(s"sum = ${sum(List(2, 3, 5, 7, 11))}")
+
+    println(s"mkString = ${mkString(List(2, 3, 5, 7))(", ")}")
   }
 }
