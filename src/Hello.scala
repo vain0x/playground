fa7553cbc08695e82677df14bdf3e1a5199c7a4e
vain0x <vainzerox@gmail.com>
2017-02-17 06:53:16 +0900

    sealed + case objects

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,46 +1,31 @@
 import scala.io._
 
+sealed abstract class DayOfWeek {
+  def next: DayOfWeek = {
+    this match {
+      case Sunday => Monday
+      case Monday => Tuesday
+      case Tuesday => Wednesday
+      case Wednesday => Thursday
+      case Thursday => Friday
+      case Friday => Saturday
+      case Saturday => Sunday
+    }
+  }
+}
+
+case object Sunday extends DayOfWeek
+case object Monday extends DayOfWeek
+case object Tuesday extends DayOfWeek
+case object Wednesday extends DayOfWeek
+case object Thursday extends DayOfWeek
+case object Friday extends DayOfWeek
+case object Saturday extends DayOfWeek
+
 object Hello {
-  def swapArray[X](array: Array[X], i: Int, j: Int) = {
-    val t = array(i)
-    array(i) = array(j)
-    array(j) = t
-  }
-
-  def joinByComma(start: Int, end: Int) = {
-    (start to end).mkString(",")
-  }
-
-  def reverse[X](list: List[X]) = {
-    list.foldLeft[List[X]](Nil)((list, x) => x :: list)
-  }
-
-  def sum(list: List[Int]) = {
-    list.foldRight(0)((x, sum) => x + sum)
-  }
-
-  def mkString[T](list: List[T])(sep: String) = {
-    list.foldLeft(("", true))((t, x) =>  {
-      val (acc, isFirst) = t
-      if (isFirst) {
-        (x.toString, false)
-      } else {
-        (acc + sep + x.toString, false)
-      }
-    })._1
-  }
-
   def main(args: Array[String]): Unit = {
-    val array = Array(0, 1, 2, 3, 4)
-    swapArray(array, 1, 3)
-    println(s"array = ${array.mkString(", ")}")
-
-    println(s"joinByComma(2, 5) = ${joinByComma(2, 5)}")
-
-    println(s"reverse: ${reverse(List(1, 2, 3, 4))}")
-
-    println(s"sum = ${sum(List(2, 3, 5, 7, 11))}")
-
-    println(s"mkString = ${mkString(List(2, 3, 5, 7))(", ")}")
+    for (dayOfWeek <- List(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)) {
+      println(s"$dayOfWeek, next = ${dayOfWeek.next}")
+    }
   }
 }
