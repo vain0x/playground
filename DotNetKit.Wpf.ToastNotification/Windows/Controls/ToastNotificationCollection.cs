da1c90c9e1d445fc7d903737a5c58a8222a550fa
vain0x <vainzerox@gmail.com>
2017-03-07 07:53:11 +0900

    Have ToastNotificationCollection capture synchronization context

--- DotNetKit.Wpf.ToastNotification/Windows/Controls/ToastNotificationCollection.cs
+++ DotNetKit.Wpf.ToastNotification/Windows/Controls/ToastNotificationCollection.cs
@@ -5,6 +5,7 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -16,6 +17,69 @@ namespace DotNetKit.Windows.Controls
     public class ToastNotificationCollection
         : ObservableCollection<ToastNotification>
     {
+        #region Synchronization
+        readonly SynchronizationContext context;
+
+        void Invoke(Action action)
+        {
+            if (context == null || context == SynchronizationContext.Current)
+            {
+                action();
+            }
+            else
+            {
+                context.Send(_ => action(), null);
+            }
+        }
+
+        /// <summary>
+        /// Clears all items in the captured synchronization context.
+        /// </summary>
+        protected override void ClearItems()
+        {
+            Invoke(() => base.ClearItems());
+        }
+
+        /// <summary>
+        /// Inserts an item in the captured synchronization context.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
+        protected override void InsertItem(int index, ToastNotification item)
+        {
+            Invoke(() => base.InsertItem(index, item));
+        }
+
+        /// <summary>
+        /// Removes an item in the captured synchronization context.
+        /// </summary>
+        /// <param name="index"></param>
+        protected override void RemoveItem(int index)
+        {
+            Invoke(() => base.RemoveItem(index));
+        }
+
+        /// <summary>
+        /// Sets an item in the captured synchronization context.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
+        protected override void SetItem(int index, ToastNotification item)
+        {
+            Invoke(() => base.SetItem(index, item));
+        }
+
+        /// <summary>
+        /// Moves an item in the captured synchronization context.
+        /// </summary>
+        /// <param name="oldIndex"></param>
+        /// <param name="newIndex"></param>
+        protected override void MoveItem(int oldIndex, int newIndex)
+        {
+            Invoke(() => base.MoveItem(oldIndex, newIndex));
+        }
+        #endregion
+
         void OnCloseRequested(object sender, EventArgs e)
         {
             Remove((ToastNotification)sender);
@@ -45,6 +109,8 @@ namespace DotNetKit.Windows.Controls
         /// </summary>
         public ToastNotificationCollection()
         {
+            context = SynchronizationContext.Current;
+
             CollectionChanged += OnCollectionChanged;
         }
     }
