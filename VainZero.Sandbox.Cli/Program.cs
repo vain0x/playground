5fd3bbd83eb9f62076e54bc5c2fa9c68b489b651
vain0x <vainzerox@gmail.com>
2017-03-28 08:42:40 +0900

    Add project

---
+++ VainZero.Sandbox.Cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace VainZero.Sandbox
+{
+    public sealed class Program
+    {
+        public void Run()
+        {
+        }
+
+        public static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+    }
+}
