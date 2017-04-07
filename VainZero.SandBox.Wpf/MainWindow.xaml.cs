9518bd858d3055cc59f383f3c1b615b0ea2d8f57
vain0x <vainzerox@gmail.com>
2017-04-07 19:24:20 +0900

    Update

--- VainZero.SandBox.Wpf/MainWindow.xaml.cs
+++ VainZero.SandBox.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,53 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Reactive.Bindings;
 
 namespace VainZero.SandBox.Wpf
 {
+    public sealed class Behavior<TSource, TTarget>
+        : INotifyPropertyChanged
+        , INotifyDataErrorInfo
+    {
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool HasErrors
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class Person
+    {
+        public string Name { get; }
+        public ReactiveProperty<int> Age { get; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = new ReactiveProperty<int>(age);
+        }
+    }
+
+    public sealed class ViewModel
+    {
+        public ReactiveProperty<string> Text { get; } =
+            new ReactiveProperty<string>("hello");
+
+        public IReadOnlyList<Person> Perons { get; } =
+
+    }
+
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
