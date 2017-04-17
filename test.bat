bc41112ad8798185e2f85e27a9d0cf551dcc930f
vain0x <vainzerox@gmail.com>
2017-04-18 00:08:08 +0900

    Install Persimmon 2.0.1

---
+++ test.bat
@@ -0,0 +1,4 @@
+"packages\Persimmon.Console.2.0.1\tools\Persimmon.Console.exe" ^
+  "MicroStream.Core.UnitTest\bin\Debug\MicroStream.Core.UnitTest.dll" ^
+  "MicroStream.Cli.UnitTest\bin\Debug\MicroStream.Cli.UnitTest.dll" ^
+  "MicroStream.Desktop.Model.UnitTest\bin\Debug\MicroStream.Desktop.Model.UnitTest.dll" ^
