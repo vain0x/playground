191840c5b8a6e6f31efdb0fa242203f54aede070
vain0x <vainzerox@gmail.com>
2017-04-18 22:04:10 +0900

    Add Database.generateIdAsync

---
+++ MicroStream.Core/Data.Entity.fs
@@ -0,0 +1,21 @@
+﻿namespace MicroStream.Data.Entity
+
+open System.Data.Entity
+
+module DbContext =
+  let saveAsync (context: DbContext) =
+    async {
+      let! cancellationToken = Async.CancellationToken
+      let! _ = context.SaveChangesAsync(cancellationToken) |> Async.AwaitTask
+      return ()
+    }
+
+module Database =
+  let generateIdAsync (database: IDatabase) =
+    async {
+      use context = database.Connect()
+      let sequence = Sequence()
+      context.Set<Sequence>().Add(sequence) |> ignore
+      do! context |> DbContext.saveAsync
+      return sequence.Id
+    }
