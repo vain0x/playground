5c23bf79bbd092423e21304fa7b80dd08d4d7674
vain0x <vainzerox@gmail.com>
2017-04-21 08:35:48 +0900

    Update

---
+++ VainZero.SandBox.Wpf/ScaleControl.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VainZero.SandBox.Wpf
+{
+    /// <summary>
+    /// ScaleControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ScaleControl : UserControl
+    {
+        private double scale;
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var height = 0.0;
+            var childSize = new Size(availableSize.Width, double.PositiveInfinity);
+            for (var i = 0; i < VisualChildrenCount; i++)
+            {
+                var child = (UIElement)VisualTreeHelper.GetChild(this, i);
+                child.Measure(childSize);
+                height += child.DesiredSize.Height;
+            }
+            scale = Math.Min(1.0, availableSize.Height / height);
+            return availableSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var scaleTransform = new ScaleTransform(scale, scale);
+            var height = 0.0;
+            for (var i = 0; i < VisualChildrenCount; i++)
+            {
+                var child = (UIElement)VisualTreeHelper.GetChild(this, i);
+                child.RenderTransform = scaleTransform;
+                child.Arrange(new Rect(new Point(0, scale * height), new Size(finalSize.Width / scale, child.DesiredSize.Height)));
+                height += child.DesiredSize.Height;
+            }
+
+            return finalSize;
+        }
+
+        public ScaleControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
