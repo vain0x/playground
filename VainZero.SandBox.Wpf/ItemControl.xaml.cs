bab1d417a63d4f6058320d48b8085dec0d992de8
vain0x <vainzerox@gmail.com>
2017-06-30 08:51:36 +0900

    Add LazyItemsControl

---
+++ VainZero.SandBox.Wpf/ItemControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VainZero.SandBox.Wpf
+{
+    /// <summary>
+    /// ItemControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ItemControl : UserControl
+    {
+        public ItemControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            Console.WriteLine("{0}", DataContext);
+            return base.MeasureOverride(constraint);
+        }
+    }
+}
--- VainZero.SandBox.Wpf/MainWindow.xaml.cs
+++ VainZero.SandBox.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,22 @@ namespace VainZero.SandBox.Wpf
         public MainWindow()
         {
             InitializeComponent();
+
+            var iota = Enumerable.Range(0, 100).ToArray();
+            var i = 0;
+            var nextItems =
+                new Func<IEnumerable>(() =>
+                {
+                    i++;
+                    if (i > iota.Length) return new int[0];
+                    return new[] { iota[i - 1] };
+                });
+
+            DataContext =
+                new
+                {
+                    NextItems = nextItems,
+                };
         }
     }
 }
