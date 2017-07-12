7e2ed108300dd31ac4c6eb273ca5b4828972df69
vain0x <vainzerox@gmail.com>
2017-07-12 19:54:54 +0900

    Connect to DB

--- VainZero.Sandbox.Cli/Program.cs
+++ VainZero.Sandbox.Cli/Program.cs
@@ -3,18 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace VainZero.Sandbox
 {
     public sealed class Program
     {
+        static MySqlConnection Connect()
+        {
+            var connectionString =
+                new MySqlConnectionStringBuilder()
+                {
+                    Server = "localhost",
+                    Database = "sandbox",
+                    UserID = "root",
+                    Password = "root",
+                }.ToString();
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        readonly MySqlConnection connection;
+
         public void Run()
         {
         }
 
+        public Program(MySqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
         public static void Main(string[] args)
         {
-            new Program().Run();
+            using (var connection = Connect())
+            {
+                new Program(connection).Run();
+            }
         }
     }
 }
