9f091f50000663dd64d59c29ddc0c69efe18abf7
vain0x <vainzerox@gmail.com>
2017-07-18 23:04:11 +0900

    Sample: Create databases asynchronously

--- VainZero.Sandbox.Cli/Program.cs
+++ VainZero.Sandbox.Cli/Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VainZero.Sandbox
 {
+    public class Person
+    {
+        [Key]
+        [Column]
+        public long Id { get; set; }
+
+        [Column]
+        [StringLength(1024)]
+        public string Name { get; set; }
+    }
+
+    public sealed class MyContext
+        : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public MyContext()
+            : base(new Effort.Provider.EffortConnection() { ConnectionString = "Data Source=in-process;IsTransient=true" }, contextOwnsConnection: true)
+        {
+        }
+    }
+
     public sealed class Program
     {
+        public string Do(int type)
+        {
+            using (var context = new MyContext())
+            {
+                // Add records.
+                if (type % 2 == 0)
+                {
+                    context.Persons.Add(new Person() { Name = "Miku" });
+                    context.Persons.Add(new Person() { Name = "Rin" });
+                    context.Persons.Add(new Person() { Name = "Luka" });
+                }
+                else
+                {
+                    context.Persons.Add(new Person() { Name = "GUMI" });
+                    context.Persons.Add(new Person() { Name = "Gakupo" });
+                }
+                context.SaveChanges();
+
+                // Gets all records.
+                var persons = context.Persons.ToArray();
+                return string.Join(", ", persons.Select(p => p.Name));
+            }
+        }
+
         public void Run()
         {
+            var tasks =
+                Enumerable.Range(0, 100)
+                .Select(i => Task.Run(() => Do(i)))
+                .ToArray();
+
+            foreach (var result in Task.WhenAll(tasks).Result)
+            {
+                Console.WriteLine(result);
+            }
         }
 
         public static void Main(string[] args)
