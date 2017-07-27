2a3b63a659e36bfcf4556cad24ba9441ca539293
vain0x <vainzerox@gmail.com>
2017-07-27 20:55:10 +0900

    WIP

---
+++ VainZero.SandBox.Wpf/TaskControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VainZero.SandBox.Wpf
+{
+    /// <summary>
+    /// TaskControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TaskControl : UserControl
+    {
+        public TaskControl()
+        {
+            InitializeComponent();
+        }
+
+        public sealed class Status
+        {
+            public Visibility ContentVisibility { get; set; }
+            public object Content { get; set; }
+            public DataTemplate ContentDataTemplate { get; set; }
+            public DataTemplateSelector ContentDataTemplateSelector { get; set; }
+
+            public Visibility BusyIndicatorVisibility { get; set; }
+            public ICommand CancelCommand { get; set; }
+        }
+    }
+}
