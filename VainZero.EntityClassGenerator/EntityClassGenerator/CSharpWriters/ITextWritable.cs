30b0aadca86a78ac761fad44b8bd7ccc4d7e3b40
vain0x <vainzerox@gmail.com>
2017-08-09 19:58:57 +0900

    Add abstractions: ITextWritable, CSharpWriter

---
+++ VainZero.EntityClassGenerator/EntityClassGenerator/CSharpWriters/ITextWritable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VainZero.EntityClassGenerator.CSharpWriters
+{
+    public interface ITextWritable
+    {
+        void Write(TextWriter writer);
+    }
+
+    public static class TextWritableExtension
+    {
+        public static void WriteToFile(this ITextWritable @this, FileInfo file)
+        {
+            using (var stream = file.OpenWrite())
+            using (var writer = new StreamWriter(stream))
+            {
+                stream.SetLength(0);
+                @this.Write(writer);
+            }
+        }
+
+        public static string GetString(this ITextWritable @this)
+        {
+            using (var writer = new StringWriter())
+            {
+                @this.Write(writer);
+                return writer.ToString();
+            }
+        }
+    }
+}
