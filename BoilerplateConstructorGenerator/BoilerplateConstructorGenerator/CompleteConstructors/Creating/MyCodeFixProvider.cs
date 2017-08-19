0260162dcae6692958a304167d9d2ee3e53ad6a2
vain0 <id_vain@yahoo.co.jp>
2017-08-19 23:21:36 +0900

    Backup

--- BoilerplateConstructorGenerator/BoilerplateConstructorGenerator/CompleteConstructors/Creating/MyCodeFixProvider.cs
+++ BoilerplateConstructorGenerator/BoilerplateConstructorGenerator/CompleteConstructors/Creating/MyCodeFixProvider.cs
@@ -29,9 +29,60 @@ namespace BoilerplateConstructorGenerator.CompleteConstructors.Creating
             return WellKnownFixAllProviders.BatchFixer;
         }
 
-        public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            return MyCodeFixer.Register(context);
+            var diagnostic = context.Diagnostics.FirstOrDefault();
+            if (diagnostic == null) return;
+
+            var document = context.Document;
+            var cancellationToken = context.CancellationToken;
+
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            if (root == null) return;
+
+            var typeDecl =
+                root
+                .FindToken(diagnostic.Location.SourceSpan.Start)
+                .Parent
+                .AncestorsAndSelf()
+                .OfType<TypeDeclarationSyntax>()
+                .FirstOrDefault();
+            if (typeDecl == null) return;
+
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+            if (semanticModel == null) return;
+
+            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+            if (typeSymbol == null) return;
+
+            async Task<Document> FixAsync(CancellationToken ct)
+            {
+                var languageVersion =
+                    (typeDecl.SyntaxTree.Options as CSharpParseOptions)?.LanguageVersion
+                    ?? LanguageVersion.CSharp6;
+                var factory = new MySyntaxFactory(languageVersion);
+
+                var varMembers = new VariableMemberCollector(semanticModel).Collect(typeDecl);
+
+                var constructor =
+                    factory.CompleteConstructor(semanticModel, typeDecl, varMembers);
+
+                return
+                    document.WithSyntaxRoot(
+                        root.ReplaceNode(
+                            typeDecl,
+                            typeDecl.AddMembers(constructor)
+                        ));
+            }
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    diagnostic.Descriptor.Title.ToString(),
+                    new Func<CancellationToken, Task<Document>>(FixAsync),
+                    equivalenceKey: diagnostic.Id
+                ),
+                diagnostic
+            );
         }
     }
 }
