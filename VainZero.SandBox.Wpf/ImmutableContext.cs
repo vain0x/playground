7bf7096db07a28974eaf5a71bb7890a1930871f5
vain0x <vainzerox@gmail.com>
2017-09-05 20:09:26 +0900

    Backup

---
+++ VainZero.SandBox.Wpf/ImmutableContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace VainZero.SandBox.Wpf
+{
+    public static class ImmutableContext
+    {
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.RegisterAttached(
+                "ItemsSource",
+                typeof(IEnumerable),
+                typeof(ImmutableContext),
+                new PropertyMetadata(OnItemsSourceChanged)
+            );
+
+        public static IEnumerable GetItemsSource(DependencyObject obj)
+        {
+            return (IEnumerable)obj.GetValue(ItemsSourceProperty);
+        }
+
+        public static void SetItemsSource(DependencyObject obj, IEnumerable value)
+        {
+            obj.SetValue(ItemsSourceProperty, value);
+        }
+
+        static void OnItemsSourceChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var itemsControl = sender as ItemsControl;
+            if (itemsControl == null) throw new InvalidOperationException();
+
+            var collection = itemsControl.ItemsSource as ObservableCollection<object>;
+            if (itemsControl.ItemsSource == null)
+            {
+                collection = new ObservableCollection<object>();
+                itemsControl.ItemsSource = collection;
+            }
+
+            // 差分を計算する。
+            // TODO: ちゃんと実装する。
+            var newItems = ((IEnumerable)e.NewValue ?? new object[0]).Cast<object>().ToArray();
+            var oldItems = ((IEnumerable)e.OldValue ?? new object[0]).Cast<object>().ToArray();
+
+            System.Diagnostics.Debug.Write("New = [" + string.Join(", ", newItems) + "]");
+            System.Diagnostics.Debug.Write("Old = [" + string.Join(", ", oldItems) + "]");
+
+            foreach (var oldItem in oldItems.Except(newItems))
+            {
+                collection.Remove(oldItem);
+            }
+            foreach (var newItem in newItems.Except(oldItems))
+            {
+                collection.Add(newItem);
+            }
+        }
+    }
+}
--- VainZero.SandBox.Wpf/MainWindow.xaml.cs
+++ VainZero.SandBox.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Reactive.Concurrency;
@@ -19,6 +20,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Prism.Commands;
 using Reactive.Bindings;
 
 namespace VainZero.SandBox.Wpf
@@ -31,6 +33,101 @@ namespace VainZero.SandBox.Wpf
         public MainWindow()
         {
             InitializeComponent();
+
+            Main =
+                new ReactiveProperty<MainView>(
+                    new MainView(
+                        new DelegateCommand(() =>
+                        {
+                            Main.Value =
+                                Main.Value.WithItems(
+                                    Main.Value.Items.Concat(new[] { new Item("hello") }).ToArray()
+                                );
+                        }),
+                        new Item[0]
+                    ));
+
+            Main.Subscribe(main =>
+            {
+                for (var i = 0; i < main.Items.Count; i++)
+                {
+                    var index = i;
+                    var item = main.Items[i];
+                    item.TextChanged += (sender, text) =>
+                    {
+                        Main.Value = main.WithItems(main.Items.WithItem(index, item.WithText(text)));
+                    };
+                }
+            });
+
+            DataContext = Main;
+        }
+
+        public ReactiveProperty<MainView> Main { get; }
+    }
+
+    public static class ListExtension
+    {
+        public static IReadOnlyList<X> WithItem<X>(this IReadOnlyList<X> @this, int index, X value)
+        {
+            return @this.Take(index).Concat(new[] { value }).Concat(@this.Skip(index + 1)).ToArray();
+        }
+    }
+
+    public sealed class Item
+    {
+        public event EventHandler<string> TextChanged;
+
+        readonly string text;
+        public string Text
+        {
+            get { return text; }
+            set { TextChanged?.Invoke(this, value); }
+        }
+
+        public Item WithText(string text)
+        {
+            return new Item(text);
+        }
+
+        public override string ToString()
+        {
+            return $"{{Text = {text}}}";
+        }
+
+        // analyzer: complete-constructor
+        public Item(string text)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+            this.text = text;
+        }
+    }
+
+    public sealed class MainView
+    {
+        public DelegateCommand AddCommand { get; }
+        public IReadOnlyList<Item> Items { get; }
+
+        public MainView WithItems(IReadOnlyList<Item> items)
+        {
+            return new MainView(AddCommand, items);
+        }
+
+        public override string ToString()
+        {
+            return $"{{Items = {Items.Count}}}";
+        }
+
+        // analyzer: complete-constructor
+        public MainView(DelegateCommand addCommand, IReadOnlyList<Item> items)
+        {
+            if (addCommand == null)
+                throw new ArgumentNullException(nameof(addCommand));
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+            AddCommand = addCommand;
+            Items = items;
         }
     }
 }
