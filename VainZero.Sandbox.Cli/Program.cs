91d4cffcf2d0465352d2a24d7d64aa0be3e49030
vain0x <vainzerox@gmail.com>
2017-09-08 21:12:49 +0900

    Replace script

--- VainZero.Sandbox.Cli/Program.cs
+++ VainZero.Sandbox.Cli/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,56 @@ namespace VainZero.Sandbox
 {
     public sealed class Program
     {
+        void OnFile(FileInfo file)
+        {
+            if (file.Extension == ".exe") return;
+
+            var content = File.ReadAllText(file.FullName);
+            var newContent =
+                content
+                .Replace("Reactive.Behaviors", "Reactive.Bindables")
+                .Replace("Behavior", "Bindable")
+                .Replace("behavior", "bindable")
+                .Replace("ビヘイビアー", "バインダブル");
+            if (content != newContent)
+            {
+                File.WriteAllText(file.FullName, newContent);
+            }
+
+            var name = file.Name;
+            var newName = name.Replace("Behavior", "Bindable");
+            if (name != newName)
+            {
+                file.MoveTo(Path.Combine(file.Directory.FullName, newName));
+            }
+        }
+
+        void OnDirectory(DirectoryInfo dir)
+        {
+            foreach (var file in dir.EnumerateFiles())
+            {
+                OnFile(file);
+            }
+
+            foreach (var subdir in dir.EnumerateDirectories())
+            {
+                if (new[] { ".git", "documents", "bin", "obj" }.Contains(subdir.Name)) continue;
+
+                OnDirectory(subdir);
+
+                var name = subdir.Name;
+                var newName = name.Replace("Behavior", "Bindable");
+                if (name != newName)
+                {
+                    subdir.MoveTo(Path.Combine(subdir.Parent.FullName, newName));
+                }
+            }
+        }
+
         public void Run()
         {
+            var root = new DirectoryInfo(@"C:\repo\DotNetKit.Reactive");
+            OnDirectory(root);
         }
 
         public static void Main(string[] args)
