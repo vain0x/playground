b87793a023649b15871ac95162de2a7eefb2a908
vain0x <vainzerox@gmail.com>
2017-09-14 20:49:10 +0900

    Backup

--- VainZero.SandBox.Wpf/MainWindow.xaml.cs
+++ VainZero.SandBox.Wpf/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
 using Reactive.Bindings;
 
 namespace VainZero.SandBox.Wpf
@@ -26,11 +28,56 @@ namespace VainZero.SandBox.Wpf
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            Loaded += (sender, e) => CaptureAsync();
+        }
+
+        public ReactiveCollection<string> Messages { get; } = new ReactiveCollection<string>();
+
+        void Log(string message)
+        {
+            Messages.Add(message);
+        }
+
+        async Task CaptureAsync()
+        {
+            try
+            {
+                var capture = new VideoCapture(0);
+                using (var win = new OpenCvSharp.Window("capture"))
+                using (var mat = new Mat())
+                {
+                    while (true)
+                    {
+                        capture.Read(mat);
+                        if (mat.Empty())
+                        {
+                            Log("empty");
+                            break;
+                        }
+
+                        win.ShowImage(mat);
+                        await Task.Delay(100);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Messages.Add(ex.ToString());
+            }
+            Log("End Capture.");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CaptureAsync();
         }
     }
 }
