2e73bb69fcdf49a952cb44c2c30d8381dd26d585
vain0x <vainzerox@gmail.com>
2017-09-28 22:21:51 +0900

    Add projects

---
+++ VainZero.Scripts.Twitter.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace VainZero.Scripts.Twitter
+{
+    sealed class Program
+    {
+        public Task RunAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        static Task Main(string[] args)
+        {
+            return new Program().RunAsync();
+        }
+    }
+}
