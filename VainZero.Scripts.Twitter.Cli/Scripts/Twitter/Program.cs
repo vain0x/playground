4737947a654afacf7efc2f2ef3e34b38a75b17e3
vain0x <vainzerox@gmail.com>
2017-10-04 21:47:44 +0900

    Backup

--- VainZero.Scripts.Twitter.Cli/Scripts/Twitter/Program.cs
+++ VainZero.Scripts.Twitter.Cli/Scripts/Twitter/Program.cs
@@ -4,49 +4,19 @@ using System.Threading.Tasks;
 using Tweetinvi;
 using Tweetinvi.Models;
 using VainZero.Scripts.Twitter.Authentication;
+using VainZero.Scripts.Twitter.SelfManagement;
 
 namespace VainZero.Scripts.Twitter
 {
     sealed class Program
     {
-        ITwitterCredentials cred;
-
-        public async Task AddListFromUserFriends()
-        {
-            //var userId = "rkgk_mochi_112";
-            foreach (var targetName in new[] { "zombie_haruna", "zombie_you" })
-            {
-
-                var me = User.GetAuthenticatedUser(cred);
-                var listName = $"home-{targetName}";
-                var twitterList = TwitterList.CreateList(listName, PrivacyMode.Private, "Just for test of my application.");
-
-                var target = User.GetUserFromScreenName(targetName);
-
-                if (targetName.Contains("haruna"))
-                {
-                    var users = User.GetFriendIds(target, maxFriendsToRetrieve: 100);
-
-                    foreach (var userId in users)
-                    {
-                        var result = twitterList.AddMember(userId);
-                        Debug.WriteLine(result);
-                    }
-                }
-                else
-                {
-                    continue;
-                }
-            }
-        }
-
         public async Task RunAsync()
         {
             try
             {
-                cred = new Authenticator().LoginAsVain0x();
+                var cred = new Authenticator().LoginAsVain0x();
 
-                await AddListFromUserFriends();
+                await new RemoveOldTweetsFunc(cred).RemoveOldTweetsAsync();
             }
             catch (Exception ex)
             {
---
+++ VainZero.Scripts.Twitter.Cli/Scripts/Twitter/SelfManagement/RemoveOldTweetsFunc.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace VainZero.Scripts.Twitter.SelfManagement
+{
+    public sealed class RemoveOldTweetsFunc
+    {
+        readonly ITwitterCredentials cred;
+
+        public RemoveOldTweetsFunc(ITwitterCredentials cred)
+        {
+            this.cred = cred ?? throw new ArgumentNullException(nameof(cred));
+        }
+
+        public async Task RemoveOldTweetsAsync()
+        {
+            RateLimit.RateLimitTrackerMode = RateLimitTrackerMode.TrackAndAwait;
+
+            var dueDate = new DateTime(2011, 1, 1);
+
+            /*
+            var maxTweetSearch = Search.SearchTweets(new Tweetinvi.Parameters.SearchTweetsParameters("from:vain0x")
+            {
+                TweetSearchType = Tweetinvi.Parameters.TweetSearchType.OriginalTweetsOnly,
+                Since = dueDate.AddDays(-3),
+                Until = dueDate,
+            });
+            var maxTweetId =
+                maxTweetSearch
+                .Select(tweet => Tuple.Create(tweet.CreatedAt, tweet.Id))
+                .Max()
+                .Item2;
+            */
+            var maxTweetId = 20537533599326208L;
+
+            var parameter = new Tweetinvi.Parameters.UserTimelineParameters()
+            {
+                MaxId = maxTweetId,
+            };
+            var me = await UserAsync.GetAuthenticatedUser(cred);
+            var tweets = await me.GetUserTimelineAsync(parameter);
+
+
+
+            foreach (var tweet in tweets)
+            {
+                if (tweet.CreatedAt >= dueDate)
+                {
+                    Debug.Assert(false);
+                }
+
+                Debug.WriteLine(tweet.Text.Substring(40));
+            }
+
+            if (Console.ReadLine() == "Y")
+            {
+                foreach (var tweet in tweets)
+                {
+                    try
+                    {
+                        if (tweet.CreatedBy != me)
+                        {
+                            Debug.WriteLine("Not mine: " + tweet.Url);
+                            continue;
+                        }
+
+                        if (tweet.IsTweetDestroyed)
+                        {
+                            Debug.WriteLine("DESTROYED: " + tweet.Url);
+                            continue;
+                        }
+
+                        if (tweet.Media?.Count > 0)
+                        {
+                            Debug.WriteLine("has-media: " + tweet.Url);
+                            continue;
+                        }
+
+                        if (tweet.IsRetweet)
+                        {
+                            tweet.UnRetweet();
+                        }
+                        else
+                        {
+                            await tweet.DestroyAsync();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                        Debug.Assert(false);
+
+                        Console.WriteLine("Stop?");
+                        if (Console.ReadLine() == "N") return;
+                    }
+                }
+            }
+        }
+    }
+}
