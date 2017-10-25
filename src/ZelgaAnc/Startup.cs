e14de0db43c4fad1019f492d3e5f8d6fb82886a6
vain0x <vainzerox@gmail.com>
2017-10-25 22:53:38 +0900

    Backup

--- src/ZelgaAnc/Startup.cs
+++ src/ZelgaAnc/Startup.cs
@@ -21,6 +21,9 @@ namespace ZelgaAnc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("name")
+            );
             services.AddMvc();
         }
 
