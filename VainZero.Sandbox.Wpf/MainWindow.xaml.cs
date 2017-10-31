4163f4b7e87ebb1dcbc02a9cd61bae28cf4c2177
vain0x <vainzerox@gmail.com>
2017-10-31 19:49:01 +0900

    Backup

--- VainZero.Sandbox.Wpf/MainWindow.xaml.cs
+++ VainZero.Sandbox.Wpf/MainWindow.xaml.cs
@@ -33,6 +33,74 @@ namespace VainZero.Sandbox.Wpf
         public MainWindow()
         {
             InitializeComponent();
+
+            var sortCommand = new ReactiveCommand<DataGridColumn>();
+            sortCommand.Subscribe(e =>
+            {
+                e.SortDirection = e.SortDirection == System.ComponentModel.ListSortDirection.Ascending ? System.ComponentModel.ListSortDirection.Descending : System.ComponentModel.ListSortDirection.Ascending;
+            });
+
+            DataContext =
+                new
+                {
+                    SortCommand = sortCommand,
+                    Items =
+                        Enumerable.Range(0, 10)
+                        .Select(i => new { Id = i, Name = $"Person {i + 1}" })
+                        .ToArray(),
+                };
         }
+    }
+
+    public static class DataGridPaginationExtension
+    {
+        #region SortCommand
+        public static readonly DependencyProperty SortCommandProperty =
+            DependencyProperty.RegisterAttached(
+                "SortCommand",
+                typeof(ICommand),
+                typeof(DataGridPaginationExtension),
+                new PropertyMetadata()
+                {
+                    PropertyChangedCallback = OnSortCommandChanged,
+                });
+
+        public static ICommand GetSortCommand(DependencyObject obj)
+        {
+            return (ICommand)obj.GetValue(SortCommandProperty);
+        }
+
+        public static void SetSortCommand(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(SortCommandProperty, value);
+        }
+
+        private static void OnDataGridSorting(object sender, DataGridSortingEventArgs e)
+        {
+            var dataGrid = (DataGrid)sender;
+            var sortCommand = GetSortCommand(dataGrid);
+            if (sortCommand != null && sortCommand.CanExecute(e))
+            {
+                e.Handled = true;
+                sortCommand.Execute(e.Column);
+            }
+        }
+
+        private static void OnSortCommandChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var dataGrid = (DataGrid)obj;
+            var oldCommand = e.OldValue as ICommand;
+            var newCommand = e.NewValue as ICommand;
+
+            if (oldCommand != null)
+            {
+                dataGrid.Sorting -= OnDataGridSorting;
+            }
+            else if (newCommand != null)
+            {
+                dataGrid.Sorting += OnDataGridSorting;
+            }
+        }
+        #endregion
     }
 }
