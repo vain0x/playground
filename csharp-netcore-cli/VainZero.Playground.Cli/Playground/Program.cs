a97508a35905c3ebd52f6d9515a03dc93fdfe969
vain0x <vainzerox@gmail.com>
2017-11-05 23:39:00 +0900

    Write stub

---
+++ csharp-netcore-cli/VainZero.Playground.Cli/Playground/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VainZero.Playground
+{
+    public sealed class Program
+    {
+        public void EntryPoint()
+        {
+        }
+
+        public static void Main(string[] args)
+        {
+            new Program().EntryPoint();
+        }
+    }
+}
