e3e00ee73cba1e3205cd67a40a60141b53a8c5e9
vain0x <vainzerox@gmail.com>
2017-11-14 07:16:27 +0900

    Add DialogHostSample

---
+++ play/wpf-material-design/VainZero.Playground.Wpf/Playground/DialogHostSampleControl.xaml.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace VainZero.Playground
+{
+    /// <summary>
+    /// DialogHostSampleControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DialogHostSampleControl : UserControl
+    {
+        public DialogHostSampleControl()
+        {
+            InitializeComponent();
+        }
+
+        #region DialogProperty
+        public static readonly DependencyProperty DialogProperty =
+            DependencyProperty.RegisterAttached(
+                "Dialog",
+                typeof(IDialog),
+                typeof(DialogHostSampleControl),
+                new FrameworkPropertyMetadata()
+                {
+                    BindsTwoWayByDefault = true,
+                    PropertyChangedCallback = OnDialogPropertyChanged,
+                });
+
+        public static IDialog GetDialog(DependencyObject obj)
+        {
+            return (IDialog)obj.GetValue(DialogProperty);
+        }
+
+        public static void SetDialog(DependencyObject obj, object value)
+        {
+            obj.SetValue(DialogProperty, value);
+        }
+
+        private static void OnDialogOpened(object sender, DialogOpenedEventArgs eventArgs)
+        {
+            var host = (DialogHost)sender;
+            var dialog = GetDialog(host);
+
+            dialog.OnOpened();
+        }
+
+        private static void OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
+        {
+            var host = (DialogHost)sender;
+            var dialog = GetDialog(host);
+
+            if (eventArgs.IsCancelled) return;
+
+            // Can cancel.
+            dialog.OnClosing(eventArgs.Cancel);
+
+            if (!eventArgs.IsCancelled)
+            {
+                SetDialog(host, null);
+            }
+        }
+
+        private static void OnDialogPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var host = (DialogHost)sender;
+            var oldDialog = (IDialog)e.OldValue;
+            var newDialog = (IDialog)e.NewValue;
+
+            if (oldDialog == null && newDialog != null)
+            {
+                host.DialogOpened += OnDialogOpened;
+                host.DialogClosing += OnDialogClosing;
+            }
+
+            if (oldDialog != null && newDialog == null)
+            {
+                host.DialogOpened -= OnDialogOpened;
+                host.DialogClosing -= OnDialogClosing;
+            }
+
+            if (oldDialog != null)
+            {
+                const object Parameter = null;
+                DialogHost.CloseDialogCommand.Execute(Parameter, host);
+
+                oldDialog.OnClosed();
+            }
+
+            if (newDialog != null)
+            {
+                host.DialogContent = newDialog;
+                host.IsOpen = true;
+            }
+        }
+        #endregion
+    }
+}
