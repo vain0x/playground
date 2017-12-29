2d7790a96a65910ff080e139bf5eb3718daf682c
vain0x <vainzerox@gmail.com>
2017-12-29 23:44:26 +0900

    Add music-library-dump script

---
+++ play/2017-12-29-music-library-dump/VainZero.Playground.Cli/Program.fs
@@ -0,0 +1,18 @@
+namespace VainZero.Playground
+
+open System
+open System.Collections
+open System.Collections.Generic
+open System.Diagnostics
+open System.IO
+open System.Net
+open System.Reflection
+open System.Web
+open System.Text
+open System.Threading
+
+module Program =
+  [<EntryPoint>]
+  let main argv =
+
+    0
