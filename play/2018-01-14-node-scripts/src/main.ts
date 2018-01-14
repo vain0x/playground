67248eabc3f9a0eafc8a3be48fa6b0b5044addcd
vain0x <vainzerox@gmail.com>
2018-01-15 01:21:00 +0900

    Add framework for batch scripts

--- play/2018-01-14-node-scripts/src/main.ts
+++ play/2018-01-14-node-scripts/src/main.ts
@@ -1,3 +1,24 @@
-export const main = () => {
-  console.log("Hello, world!");
+import { BatchApp, BatchCommand } from "./batch-app-framework";
+
+const commands: BatchCommand[] = [
+  {
+    verb: "help",
+    alias: ["-h", "--help"],
+    help() {
+      return ["Print usage."];
+    },
+    async run(args, app) {
+      app.printUsage();
+    },
+  },
+  {
+    verb: "hello",
+    async run(args) {
+      console.log("Hello, world!");
+    },
+  },
+];
+
+export const main = async () => {
+  return await new BatchApp(commands).main();
 };
