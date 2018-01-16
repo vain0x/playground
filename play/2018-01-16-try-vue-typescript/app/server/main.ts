b8396f234f8b3c689b224cfb3f9919badf284e1b
vain0x <vainzerox@gmail.com>
2018-01-16 21:00:36 +0900

    Update

--- play/2018-01-16-try-vue-typescript/app/server/main.ts
+++ play/2018-01-16-try-vue-typescript/app/server/main.ts
@@ -1,4 +1,5 @@
 import * as Q from "typescript";
+import * as E from "linq";
 
 // const t = HTMLElement;
 const f = Array.from;
@@ -6,5 +7,8 @@ const f = Array.from;
 const a = async () => {
   return 1;
 };
+
+export const write = (p: (x: number) => void) => E.range(1, 10).toArray().forEach(x => p(x));
+
 
 export { a, f };
