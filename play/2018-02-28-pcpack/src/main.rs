3dc72d66d2b8038a7b304676d4e4cc9a43cba176
vain0x <vainzerox@gmail.com>
2018-03-01 23:07:19 +0900

    Update

--- play/2018-02-28-pcpack/src/main.rs
+++ play/2018-02-28-pcpack/src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+use std::*;
+use std::io::prelude::*;
+use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+
+fn go() -> Result<(), io::Error> {
+    let library_path = "src";
+    let entries = try!(fs::read_dir(library_path));
+
+    for entry in entries {
+        let entry = try!(entry);
+        let path = entry.file_name();
+        println!("{:?}", path);
+    }
+
+    return Ok(());
+}
+
 fn main() {
-    println!("Hello, world!");
+    go().unwrap();
+
+    // let mut f = File::open(filename).expect("file not found");
+
+    // let mut contents = String::new();
+    // f.read_to_string(&mut contents)
+    // .expect("something went wrong reading the file");
+    //
+    // println!("With text:\n{}", contents);
 }
