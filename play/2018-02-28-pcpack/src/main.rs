c455fcbadf90882ad84a1bce0bd857d223f24ce4
vain0x <vainzerox@gmail.com>
2018-03-01 23:59:40 +0900

    Parse toml

--- play/2018-02-28-pcpack/src/main.rs
+++ play/2018-02-28-pcpack/src/main.rs
@@ -1,18 +1,68 @@
+extern crate toml;
+
 use std::env;
 use std::*;
+use std::collections::BTreeMap;
 use std::io::prelude::*;
 use std::io;
 use std::fs::{self, DirEntry};
 use std::path::Path;
 
-fn go() -> Result<(), io::Error> {
-    let library_path = "src";
-    let entries = try!(fs::read_dir(library_path));
+struct Module {
+    name: String,
+    path: String,
+}
 
-    for entry in entries {
-        let entry = try!(entry);
-        let path = entry.file_name();
-        println!("{:?}", path);
+#[derive(Debug)]
+enum MyError {
+    IOError(io::Error),
+    ParseError(toml::de::Error),
+}
+
+impl convert::From<io::Error> for MyError {
+    fn from(error: io::Error) -> MyError {
+        return MyError::IOError(error);
+    }
+}
+
+impl convert::From<toml::de::Error> for MyError {
+    fn from(error: toml::de::Error) -> MyError {
+        return MyError::ParseError(error);
+    }
+}
+
+fn go() -> Result<(), MyError> {
+    // Move working directory into `workspace`.
+    let mut work_dir = try!(env::current_dir());
+    work_dir.push("workspace");
+    try!(env::set_current_dir(&work_dir.as_path()));
+    let work_dir = work_dir.as_path();
+    eprintln!("Current dir = {}", work_dir.display());
+
+    // Read the config file.
+    let mut name_to_module: BTreeMap<String, Module> = BTreeMap::new();
+
+    let package_file_path = "pcpack-packages.toml";
+    let mut package_file = try!(fs::File::open(package_file_path));
+    let mut package_toml = String::new();
+    try!(package_file.read_to_string(&mut package_toml));
+    eprintln!("toml = {}", package_toml.to_owned());
+    let packages: toml::Value = try!(package_toml.parse());
+    eprintln!("{:?}", packages);
+    /* doc = {
+        modules: [
+            { name, path },
+            ...
+        ]
+    } */
+    let doc = packages.as_table().unwrap();
+    let modules = doc.get("modules").unwrap().as_array().unwrap();
+    for module in modules {
+        let module = module.as_table().unwrap();
+        let name = module["name"].as_str().unwrap().to_owned();
+        let path = module["path"].as_str().unwrap().to_owned();
+        eprintln!("Module '{}' in '{}'.", name, path);
+        name_to_module.insert(name.to_owned(), Module { name, path });
     }
 
     return Ok(());
@@ -28,4 +78,11 @@ fn main() {
     // .expect("something went wrong reading the file");
     //
     // println!("With text:\n{}", contents);
+}
+
+#[test]
+fn toml_test() {
+    let value = "foo = 'bar'".parse::<toml::Value>().unwrap();
+
+    assert_eq!(value["foo"].as_str(), Some("bar"));
 }
