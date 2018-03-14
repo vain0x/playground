631db217d7470e7499d45310ffeb9579d0d49401
vain0x <vainzerox@gmail.com>
2018-03-14 23:19:34 +0900

    Update sample of DotLiquid

--- play/2017-11-14-dot-liquid/VainZero.Playground.Cli/Program.fs
+++ play/2017-11-14-dot-liquid/VainZero.Playground.Cli/Program.fs
@@ -2,41 +2,15 @@ namespace VainZero.Playground
 
 open System
 open System.Collections
-open System.Collections.Generic
-open System.Diagnostics
 open System.IO
 open System.Net
-open System.Reflection
-open System.Web
-open System.Text
-open System.Threading
-open System.Runtime.InteropServices
+open System.Text.RegularExpressions
 open DotLiquid
 
 module LiquidExtension =
-  open DotLiquid
 
-  (*
-  type HtmlBlock() =
-    inherit Block()
-
-    let _escapeFilter =
-      Variable.Filter("escape", arguments = [||])
-
-    static do
-      //Template.RegisterTag<HtmlBlock>("html")
-      ()
-
-    override this.Render(context: Context, writer: TextWriter): unit =
-      for node in this.NodeList do
-        match node with
-        | :? Variable as var when var.Filters.Count = 0 ->
-          var.Filters.Add(_escapeFilter)
-        | _ -> ()
-      base.Render(context, writer)
-  *)
-
-  let hasify =
+  // roughly
+  let private deepHasify =
     let rec go (model: obj) =
       match model with
       | null
@@ -44,21 +18,33 @@ module LiquidExtension =
       | :? int
       | :? int64
       | :? float
-      | :? decimal ->
+      | :? decimal
+      | :? DateTime
+      | :? TimeSpan ->
         model
+      | :? IDictionary ->
+        NotImplementedException("TODO: Impl dictionary hasify") |> raise
+      | :? ICollection as xs ->
+        [| for x in xs -> go x |] :> obj
       | _ ->
         hasify model :> obj
     and hasify model =
-      let dict =
-        Hash.FromAnonymousObject(model)
-        |> Seq.map (fun (KeyValue (key, value)) -> (key, go value))
-        |> dict
-      Hash.FromDictionary(dict)
+      model
+      |> fun m -> Hash.FromAnonymousObject(m)
+      |> Seq.map (fun (KeyValue (key, value)) -> (key, go value))
+      |> dict
+      |> fun d -> Hash.FromDictionary(d)
     hasify
 
-  let render source (model: Hash) =
-    let template = Template.Parse(source)
-    template.Render(model)
+  let setUp () =
+    // Enable automatic HTML encoding.
+    Template.RegisterValueTypeTransformer(typeof<string>, fun m -> WebUtility.HtmlEncode(m :?> string) |> box)
+
+  let build<'T> templateSource =
+    let template = lazy Template.Parse(templateSource)
+    fun (model: 'T) ->
+      let hash = deepHasify model
+      template.Value.Render(hash)
 
 module Program =
 
@@ -77,21 +63,22 @@ module Program =
     }
 
   [<EntryPoint>]
-  let main argv =
+  let main _ =
     let (-->) k v = (k, box v)
-    // let dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
-    // let source = File.ReadAllText(Path.Combine(dir, "hello.liquid"))
 
-    // Enable automatic HTML encoding.
-    DotLiquid.Template.RegisterValueTypeTransformer(typeof<string>, fun m -> WebUtility.HtmlEncode(m :?> string) |> box)
+    LiquidExtension.setUp ()
 
     let source = File.ReadAllText("hello.liquid.html")
-    let model = { Message = { Text = "<script>John Doe</script>" } }
-    // let model = new DotLiquid.Hash()
-    // let message = new DotLiquid.Hash()
-    // message.Add("Screem", "OK!")
-    // message.Add("Silent", "yes...")
-    // model.Add("Message", message)
-    let target = LiquidExtension.render source (model |> LiquidExtension.hasify)
+    let render: Model -> string = LiquidExtension.build source
+
+    let model =
+      {
+        // XSS attack!
+        Message = { Text = "<script>John Doe</script>" }
+      }
+    let target = render model
+
     printfn "%s" target
+
+    assert (target.Contains("&lt;/SCRIPT&gt;"))
     0
