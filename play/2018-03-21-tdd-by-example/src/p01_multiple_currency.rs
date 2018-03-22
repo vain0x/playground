f065f3156f4e59999975289d4f15dc32d22222c0
vain0x <vainzerox@gmail.com>
2018-03-22 22:23:54 +0900

    Testing just for learning Rust

--- play/2018-03-21-tdd-by-example/src/p01_multiple_currency.rs
+++ play/2018-03-21-tdd-by-example/src/p01_multiple_currency.rs
@@ -16,13 +16,33 @@
 //!     - [x] 5 CHF * 2 = 10 CHF
 //!     - [ ] $5 + $5 = $10
 
+use std::collections::HashMap;
 use std::convert::Into;
 
 type Currency = &'static str;
 
-struct Bank;
+struct Bank {
+    rates: HashMap<(Currency, Currency), i32>,
+}
 
 impl Bank {
+    fn new() -> Bank {
+        Bank {
+            rates: HashMap::new(),
+        }
+    }
+
+    fn set_rate(
+        &mut self,
+        left_amount: i32,
+        left_currency: Currency,
+        right_amount: i32,
+        right_currency: Currency,
+    ) {
+        let r = right_amount / left_amount;
+        // self.rates.insert;
+    }
+
     fn reduce(&self, source: Expression, currency: Currency) -> Money {
         match source {
             Expression::Money(money) => money,
@@ -105,12 +125,25 @@ fn franc(amount: i32) -> Money {
 pub mod tests {
     use super::*;
 
+    #[cfg(a)]
+    #[test]
+    fn test_rate() {
+        let mut bank = Bank::new();
+        bank.set_rate(1, "USD", 2, "CHF");
+
+        assert_eq!(1, bank.rate("USD", "USD"));
+
+        assert_eq!(2, bank.rate("CHF", "USD"));
+    }
+
+    #[cfg(a)]
     #[test]
     fn test_reduce_dollar_to_dollar() {
         let five = dollar(5).to_expr();
         assert_eq!(dollar(5), Bank.reduce(five, "USD"));
     }
 
+    #[cfg(a)]
     #[test]
     fn test_plus() {
         let expression = dollar(6).plus(dollar(4));
@@ -142,5 +175,16 @@ pub mod tests {
     fn test_with_amount() {
         assert_eq!(dollar(8), dollar(1).with_amount(8));
         assert_eq!(franc(8), franc(1).with_amount(8));
+    }
+
+    #[test]
+    fn test_hash_map_insert() {
+        let mut map = HashMap::<(Currency, Currency), i32>::new();
+        map.insert(("CHF", "USD"), 2);
+
+        assert_eq!(Some(2), map.get(&("CHF", "USD")).cloned());
+
+        map.insert(("CHF", "USD"), 3);
+        assert_eq!(Some(3), map.get(&("CHF", "USD")).cloned());
     }
 }
