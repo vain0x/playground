150755213b63a7b832a1ef6041e2170e204f26a9
vain0x <vainzerox@gmail.com>
2018-04-13 21:54:05 +0900

    Add project stubs

---
+++ play/2018-04-13-derive-gen/DeriveGen.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeriveGen.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
