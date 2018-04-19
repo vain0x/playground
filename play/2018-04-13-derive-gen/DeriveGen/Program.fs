1305d996a54f3824d0288ec2eaadda56bb29931c
vain0x <vainzerox@gmail.com>
2018-04-20 00:53:57 +0900

    WIP

--- play/2018-04-13-derive-gen/DeriveGen/Program.fs
+++ play/2018-04-13-derive-gen/DeriveGen/Program.fs
@@ -1,6 +1,28 @@
 ﻿namespace DeriveGen
 
 module Program =
+  open System
+
+  [<RequireQualifiedAccess>]
+  type ExitCode =
+    | Ok = 0
+    | Error = 1
+
+  let viaStandardIO () =
+    async {
+      let! json =  Console.In.ReadToEndAsync() |> Async.AwaitTask
+      match json |> Processor.generate with
+      | Ok csharpCode ->
+        do! Console.Out.WriteAsync(csharpCode) |> Async.AwaitTask
+        return ExitCode.Ok
+      | Error err ->
+        let err = err |> Chiron.JsonFailure.summarize
+        do! Console.Error.WriteAsync(err) |> Async.AwaitTask
+        return ExitCode.Error
+    }
+
   [<EntryPoint>]
   let main _ =
-    0
+    viaStandardIO ()
+    |> Async.RunSynchronously
+    |> int<ExitCode>
