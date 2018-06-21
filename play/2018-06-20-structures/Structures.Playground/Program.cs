c8d590dde90b8536de83f108c09f603163c2e1ed
vain0x <vainzerox@gmail.com>
2018-06-21 23:53:12 +0900

    wip!

---
+++ play/2018-06-20-structures/Structures.Playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Structures.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
