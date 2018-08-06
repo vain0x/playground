b71e25664118ad5b6914387f61e9865d70f6c27a
vain0x <vainzerox@gmail.com>
2018-08-06 19:21:18 +0900

    Add code sketch

--- play/2018-08-06-kana-keystrokes/Program.fs
+++ play/2018-08-06-kana-keystrokes/Program.fs
@@ -1,8 +1,73 @@
-﻿// Learn more about F# at http://fsharp.org
+module Program
 
 open System
 
+let rules = [
+  "A", "あ", ""
+  "I", "い", ""
+  "U", "う", ""
+  "E", "え", ""
+  "O", "お", ""
+  "YA", "や", ""
+  "YI", "い", ""
+  "YU", "ゆ", ""
+  "YE", "いぇ", ""
+  "YO", "よ", ""
+  "XYA", "ゃ", ""
+  "XYU", "ゅ", ""
+  "XYO", "ょ", ""
+  "TT", "っ", "T"
+  "TA", "た", ""
+  "TI", "ち", ""
+  "TU", "つ", ""
+  "TE", "て", ""
+  "TO", "と", ""
+  "TYA", "ちゃ", ""
+  "TYU", "ちゅ", ""
+  "TYO", "ちょ", ""
+  "TSU", "つ", ""
+  "CC", "っ", "C"
+  "CHA", "ちゃ", ""
+  "XTU", "っ", ""
+  "XTSU", "っ", ""
+]
+
+let rec reduceComplete (source: string) (input: string) =
+  seq {
+    let mutable leaf = true
+    if source.Length > 0 && input.Length > 0 then
+      for given, target, remain in rules do
+        if input.StartsWith(given) && source.StartsWith(target) then
+          leaf <- false
+          yield! reduceComplete (source.Substring(target.Length)) (remain + input.Substring(given.Length))
+    if leaf then
+      yield source, input
+  }
+
+let rec findNext (source: string) (input: string) =
+  seq {
+    for given, target, _ in rules do
+      if given.StartsWith(input) && source.StartsWith(target) then
+        yield given, target
+  }
+
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _ =
+  let source = "つっちゃった"
+
+  let rec go () =
+    eprintf "Input: "
+    match Console.ReadLine() with
+    | null -> ()
+    | input ->
+      let input = input.ToUpperInvariant()
+
+      for source, input in reduceComplete source input do
+        printfn "(%s, %s)" source input
+        for g, t in findNext source input do
+          printfn "    (%s, %s)" g t
+
+      go ()
+
+  go ()
+  0
