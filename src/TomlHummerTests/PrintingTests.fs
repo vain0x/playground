8df6c42979bd675800b0a372b7adba77bab67958
vain0x <vainzerox@gmail.com>
2018-09-22 13:43:46 +0900

    Add test of printing inline table

--- src/TomlHummerTests/PrintingTests.fs
+++ src/TomlHummerTests/PrintingTests.fs
@@ -9,11 +9,17 @@ open TomlHummer.Printing
 let printTest () =
   let source =
     [
+      StmtSyn.Binding ([ExprSyn.Ident "core"],
+        ExprSyn.Table [
+          [ExprSyn.Ident "autocrlf"], ExprSyn.String "input"
+          [ExprSyn.Ident "excludesFile"], ExprSyn.String "~/.gitignore"
+        ])
       StmtSyn.Binding ([ExprSyn.Ident "user"; ExprSyn.Ident "name"], ExprSyn.String "John Doe")
       StmtSyn.Binding ([ExprSyn.Ident "user"; ExprSyn.Ident "age"], ExprSyn.Int 18)
     ]
   let actual = print source
-  let expected = """user.name = "John Doe"
+  let expected = """core = { autocrlf = "input", excludesFile = "~/.gitignore" }
+user.name = "John Doe"
 user.age = 18
 """
 
