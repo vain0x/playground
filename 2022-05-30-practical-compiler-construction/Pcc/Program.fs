212e7d4344e8e70d1aa9bb353d9f21375e91dc04
vain0x <vainzerox@gmail.com>
2022-06-22 00:36:35 +0900

    feat: 構文木を作る

--- 2022-05-30-practical-compiler-construction/Pcc/Program.fs
+++ 2022-05-30-practical-compiler-construction/Pcc/Program.fs
@@ -2,6 +2,12 @@ module rec Pcc.Program
 
 open System.IO
 
+/// Parse tree element.
+[<RequireQualifiedAccess; NoEquality; NoComparison>]
+type PElement =
+  | Token of name: string
+  | Node of name: string * children: PElement list
+
 [<EntryPoint>]
 let main _ =
   let args = System.Environment.GetCommandLineArgs()
@@ -9,89 +15,88 @@ let main _ =
   let grammarText = File.ReadAllText(args.[2])
   let input = stdin.ReadToEnd()
 
-  // let rules =
-  //   try
-  //     MyLex.parseLexer input
-  //   with
-  //   | MyLex.ParseLexerException (msg, row, column) ->
-  //     printfn "PARSE ERROR: %s at %d:%d" msg (row + 1) (column + 1)
-  //     exit 1
+  // Generate lexer.
+  let rules =
+    try
+      MyLex.parseLexer lexText
+    with
+    | MyLex.ParseLexerException (msg, row, column) ->
+      printfn "FATAL: Invalid lexer. %s at %d:%d" msg (row + 1) (column + 1)
+      exit 1
 
-  // // printfn "%A" rules
-  // rules
-  // |> List.iter (fun (name, term) ->
-  //   if name = "STR" || name = "ID" then
-  //     printfn "%s: %A" name term)
+  let nfa = MyLex.generateNfa rules
 
-  // let nfa = MyLex.generateNfa rules
+  // Generate parser.
+  let parser =
+    try
+      MyYacc.generateLrParser grammarText
+    with
+    | MyYacc.ParseGrammarException (msg, i) ->
+      eprintfn "FATAL: Invalid grammar. %s at %d" msg i
+      exit 1
 
-  // let u, trans, accepts = nfa
-  // printfn "%d\n%A\n%A" u trans accepts
+  // let dumpGrammar () = MyYacc.dump grammarText
 
-  // let em (input: string) =
-  //   printf "input: %A    -> " input
-  //   let result = MyLex.emulateNfa input nfa
-  //   printfn "%A" result
+  // Tokenize.
+  let tokens =
+    try
+      MyLex.tokenizeWithNfa input nfa
+    with
+    | MyLex.TokenizeException index ->
+      printfn "ERROR: Tokenize failed at %d\n" index
+      printfn "  %s" input
+      printfn "  %s^" (String.replicate index " ")
+      exit 1
 
-  // em "42"
-  // // em "-123"
-  // em "a"
-  // em "_Az09"
-  // em "if"
-  // em "iff"
-  // em "\"\""
-  // em "\"...\""
-  // em "\"Hello, world!\""
-  // em ";"
-  // em "<="
-
-  // let tok (input: string) =
-  //   printfn "tokenize: %A" input
-
-  //   let result =
-  //     try
-  //       MyLex.tokenizeWithNfa input nfa
-  //     with
-  //     | MyLex.TokenizeException index ->
-  //       printfn "ERROR: Tokenize failed at %d\n" index
-  //       printfn "  %s" input
-  //       printfn "  %s^" (String.replicate index " ")
-  //       exit 1
-
-  //   result
+  // let dumpTokens () =
+  //   tokens
   //   |> List.map (fun (kind, len) -> sprintf "%s(%d)" kind len)
   //   |> String.concat " "
   //   |> printfn "  %s"
 
-  // tok "a"
-  // tok "if"
-  // tok "if (x == 0) { sprint \"Hello, world!\"; }"
+  // Parse.
+  let tokens =
+    tokens
+    |> Seq.map fst
+    |> Seq.filter (fun name -> name <> "SPACE")
+    |> Seq.toArray
 
-  // try
-  //   MyYacc.dump input
-  // with
-  // | MyYacc.ParseGrammarException (msg, i) -> eprintfn "ERROR: %s at %d" msg i
-
-  let parser = MyYacc.generateLrParser grammarText
-  let tokens = input.Trim().Split(' ')
   let events = MyYacc.LrParser.parse (Array.toList tokens) parser
 
-  let rec go indent count events =
-    (match events with
-     | _ when count = 0 -> events
+  let dumpParseEvents events =
+    let rec go indent count events =
+      match events with
+      | _ when count = 0 -> events
 
-     | MyYacc.ParseEvent.Token i :: events ->
-       eprintfn "%s%s %s" indent "token" tokens.[i]
-       go indent (count - 1) events
+      | MyYacc.ParseEvent.Token i :: events ->
+        eprintfn "%s%s %s" indent "token" tokens.[i]
+        go indent (count - 1) events
 
-     | MyYacc.ParseEvent.StartNode (name, childrenCount) :: events ->
-       eprintfn "%s%s %s (%d)" indent "node" name childrenCount
-       let events = go (indent + "  ") childrenCount events
-       go indent (count - 1) events
+      | MyYacc.ParseEvent.StartNode (name, childrenCount) :: events ->
+        eprintfn "%s%s %s (%d)" indent "node" name childrenCount
+        let events = go (indent + "  ") childrenCount events
+        go indent (count - 1) events
 
-     | [] -> failwith "unreachable")
+      | [] -> failwith "unreachable"
 
-  let events = go "" 1 events
-  eprintfn "rest: %A" events
+    let events = go "" 1 events
+    eprintfn "rest: %A" events
+
+  let root =
+    let rec go acc count events =
+      match events with
+      | _ when count = 0 -> List.rev acc, events
+
+      | MyYacc.ParseEvent.Token i :: events -> go (PElement.Token tokens.[i] :: acc) (count - 1) events
+
+      | MyYacc.ParseEvent.StartNode (name, childrenCount) :: events ->
+        let children, events = go [] childrenCount events
+        go (PElement.Node(name, children) :: acc) (count - 1) events
+
+      | [] -> failwith "unreachable"
+
+    go [] 1 events
+
+  eprintfn "%A" root
 
   0
