f0a6aa0da98291ac7121782674fbf0aa83885cf1
vain0x <vainzerox@gmail.com>
2022-08-20 23:47:55 +0900

    feat: MIRのダンプを改善

--- 2022-08-07-optimizing-compiler/OptLang/Program.fs
+++ 2022-08-07-optimizing-compiler/OptLang/Program.fs
@@ -92,13 +92,115 @@ let private dumpMir (mir: M.MProgram) =
     locals
     |> List.map (fun (_, local: M.LocalDef) -> Label(local.Name, Text(dumpMTy mir local.Ty)))
 
+  let rec displayPlace (place: M.MPlace) =
+    let sb = StringBuilder()
+
+    sb.Append(string place.Local) |> ignore
+
+    for part in place.Path do
+      match part with
+      | M.Part.Index (index, _) ->
+        sb
+          .Append("[")
+          .Append((displayRval index: string))
+          .Append("]")
+        |> ignore
+      | M.Part.Field (index, record) ->
+        sb
+          .Append(".")
+          .Append(
+            (mir.Records |> lookup record).Fields.[index]
+            |> string
+          )
+        |> ignore
+
+    sb.ToString()
+
+  and displayRval (rval: M.MRval) =
+    match rval with
+    | M.MRval.Void -> "void"
+    | M.MRval.Bool value -> string value
+    | M.MRval.Int value -> string value
+    | M.MRval.String value -> sprintf "%A" value
+    | M.MRval.Read place -> "read " + displayPlace place
+
+    | M.MRval.Unary (unary, rval) ->
+      let unary =
+        match unary with
+        | M.MUnary.Minus -> "-"
+        | M.MUnary.Not -> "!"
+        | M.MUnary.ArrayLen -> "(.len)"
+
+      unary + displayRval rval
+
+    | M.MRval.Binary (binary, lhs, rhs) ->
+      let b =
+        match binary with
+        | M.MBinary.Add -> "+"
+        | M.MBinary.Subtract -> "-"
+        | M.MBinary.Multiply -> "*"
+        | M.MBinary.Divide -> "/"
+        | M.MBinary.Modulo -> "%"
+        | M.MBinary.Equal -> "=="
+        | M.MBinary.NotEqual -> "!="
+        | M.MBinary.LessThan -> "<"
+        | M.MBinary.LessEqual -> "<="
+        | M.MBinary.GreaterThan -> ">"
+        | M.MBinary.GreaterEqual -> ">="
+
+      "("
+      + displayRval lhs
+      + " "
+      + b
+      + " "
+      + displayRval rhs
+      + ")"
+
+    | M.MRval.Record (fields, record) ->
+      (mir.Records |> lookup record).Fields
+      |> Array.zip fields
+      |> Array.map (fun (value, field) -> field.Name + " = " + displayRval value)
+      |> String.concat "; "
+      |> (fun x -> record.Name + "{" + x + "}")
+
+    | M.MRval.Array (items, array) ->
+      let itemTy = mir.Arrays.[array.Index].ItemTy |> string
+
+      let items =
+        items
+        |> Array.map displayRval
+        |> String.concat ", "
+
+      "array(" + itemTy + ")[" + items + "]"
+
   let onStmtList stmtList =
     stmtList
     |> List.map (fun stmt ->
       match stmt with
-      | M.MStmt.Assign (dest, value) -> sprintf "%A <- %A" dest value
-      | M.MStmt.Call (callable, args) -> sprintf "call %A%A" callable args)
+      | M.MStmt.Assign (dest, value) -> sprintf "set %s <- %s" (displayPlace dest) (displayRval value)
+
+      | M.MStmt.Call (callable, args) ->
+        sprintf
+          "call %A(%s)"
+          callable
+          (args
+           |> Array.map displayRval
+           |> String.concat ", "))
     |> List.map Text
+
+  let onTerminator terminator =
+    match terminator with
+    | M.MTerminator.Unreachable -> "unreachable"
+    | M.MTerminator.Goto label -> "goto " + string label
+    | M.MTerminator.Return -> "return"
+
+    | M.MTerminator.If (cond, body, alt) ->
+      "goto if "
+      + displayRval cond
+      + " then "
+      + string body
+      + " else "
+      + string alt
 
   let onBlocks blocks =
     blocks
@@ -108,7 +210,7 @@ let private dumpMir (mir: M.MProgram) =
         List(
           List.append
             (onStmtList (Array.toList block.Stmts))
-            [ Label("terminator", Text(sprintf "%A" block.Terminator)) ]
+            [ Label("terminator", Text(onTerminator block.Terminator)) ]
         )
       ))
 
