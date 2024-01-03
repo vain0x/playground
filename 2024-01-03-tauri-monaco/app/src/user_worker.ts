fcb14c1d43e302e4bd5ce5c994e14607872c1de1
vain0x <vainzerox@gmail.com>
2024-01-03 23:50:05 +0900

    feat(tauri-monaco): monaco-editorのエディタコンポーネントを追加
    monaco-editorリポジトリにある browser-esm-vite-react のサンプルコードを参考にした
    
    > https://github.com/microsoft/monaco-editor/blob/dfca77d07111fd0ac192a15535cbdce3ee4157b8/samples/browser-esm-vite-react/src/components/Editor.tsx

---
+++ 2024-01-03-tauri-monaco/app/src/user_worker.ts
@@ -0,0 +1,29 @@
+// copied from https://github.com/microsoft/monaco-editor/blob/dfca77d07111fd0ac192a15535cbdce3ee4157b8/samples/browser-esm-vite-react/src/userWorker.ts
+
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+// @ts-ignore
+self.MonacoEnvironment = {
+  getWorker(_: any, label: string) {
+    if (label === 'json') {
+      return new jsonWorker();
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return new cssWorker();
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return new htmlWorker();
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new tsWorker();
+    }
+    return new editorWorker();
+  }
+};
+
+monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
